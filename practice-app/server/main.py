ef77bdfa2102764903fee87fa086379d5a0ca762
Begüm Arslan <begumarslan@outlook.de>
2023-05-07 19:03:15 +0300

    added CORS middleware

--- practice-app/server/main.py
+++ practice-app/server/main.py
@@ -1,9 +1,16 @@
 from fastapi import Depends, FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from routers import  user
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
     user.router,
