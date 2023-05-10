e83f45f6bf2cb6188b35b8e8ca0509b686581506
HasanBingölbali <74209102+HasanBingolbali@users.noreply.github.com>
2023-05-10 15:45:25 +0300

    Revert "Merge pull request #168 from bounswe/cahid-practice-ap"
    This reverts commit dc337e69aafd0c525330bd967858088e5e8da40b, reversing
    changes made to 791f43c9dcbb62cd9a616a8c57700708fc9caa37.

--- practice-app/server/main.py
+++ practice-app/server/main.py
@@ -1,11 +1,8 @@
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import  user
-from add_activity_api.main import app as add_activity_app
 import db
-
 app = FastAPI()
-app.include_router(add_activity_app)
 
 app.add_middleware(
     CORSMiddleware,
