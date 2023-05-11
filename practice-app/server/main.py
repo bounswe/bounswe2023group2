0aafd92cb1d99992fa9b9b04c02cb2020a33258a
mervegrbz <merve@aposto.com>
2023-05-11 22:00:35 +0300

    add news page

--- practice-app/server/main.py
+++ practice-app/server/main.py
@@ -1,6 +1,7 @@
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import user
+from routers import news
 from routers import notifications
 
 app = FastAPI()
@@ -23,6 +24,12 @@ app.include_router(
     notifications.router,
     prefix="/notifications",
     tags=["notifications"],
+)
+
+app.include_router(
+    news.router,
+    prefix="/news",
+    tags=["news"],
 )
 
 @app.get("/")
