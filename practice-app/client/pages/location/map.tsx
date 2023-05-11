9c577c1817c4050fa23bf4b7a72983bd7a4b63a3
“sahinvitamu” <sahin@vitamu.com>
2023-05-11 22:23:09 +0300

    marker fixed

--- practice-app/client/pages/location/map.tsx
+++ practice-app/client/pages/location/map.tsx
@@ -1,18 +1,30 @@
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
- 
+import * as L from 'leaflet';
+import iconMarker from 'leaflet/dist/images/marker-icon.png'
+import iconRetina from 'leaflet/dist/images/marker-icon.png'
+import iconShadow from 'leaflet/dist/images/marker-shadow.png'
+
+var greenIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
 
 const Map = () => {
   return (
     <div>
       <div style={{textAlign:"center" }}>MAP</div>
-    <MapContainer center={[51.505, -0.09]} zoom={5
-    } scrollWheelZoom={true} style={{height: 400, width: "100%"}}>
+    <MapContainer center={[37,37]} zoom={5
+    } scrollWheelZoom={true} style={{height: 800, width: "100%"}}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[37,37]} icon={greenIcon}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
