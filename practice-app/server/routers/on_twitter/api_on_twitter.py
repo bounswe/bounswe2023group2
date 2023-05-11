ee1dbdfc8e24944abb713927ef0c94c18f6a2b81
Mehmet <mehmetkuzulugil@gmail.com>
2023-05-11 21:14:40 +0300

    api on twitter main skeleton

--- practice-app/server/routers/on_twitter/api_on_twitter.py
+++ practice-app/server/routers/on_twitter/api_on_twitter.py
@@ -3,6 +3,8 @@ from .twitterSpaces import TwitterSpace
 
 router = APIRouter()
 
+
+# Sample space 1LyGBqAPMQYKN
 @router.get("/waiting", )
 async def waitingSpaces():
     return {"spacesId": "0000000"}
@@ -11,6 +13,12 @@ async def waitingSpaces():
 async def waitingSpaceWithId(spacesId: str):
     twitterSpace = TwitterSpace(spacesId)
     return {"spacesId": twitterSpace.spacesID}
+
+@router.get("/getSpace/{spacesId}", )
+async def getTwitterSpace(spacesId):
+    twitterSpace = TwitterSpace(spacesId)
+    json_data = twitterSpace.processSpace()
+    return json_data
 
 @router.post("/processSpace/{spacesId}", )
 async def processTwitterSpace(spacesId):
--- practice-app/server/routers/on_twitter/twitterSpaces.py
+++ practice-app/server/routers/on_twitter/twitterSpaces.py
@@ -1,4 +1,7 @@
 from .returncodes import *
+from TwitterAPI import TwitterAPI
+
+from .secrets import *
 class TwitterSpace():
     spacesID: str
     def __init__(self, spacesID):
@@ -8,7 +11,23 @@ class TwitterSpace():
             return self.fetchFromTwitter()
 
     def fetchFromTwitter(self):
+        r = twitter_api.request('spaces', {'id': self.spacesID})
+        print(self.spacesID)
+        return r
+    def tatava(self):
         json_data = {'Kod': KOD_OK }
         return json_data
+def createTwitterAPI():
+    api = TwitterAPI(CONSUMER_KEY,
+                     CONSUMER_SECRET,
+                     ACCESS_TOKEN,
+                     ACCESS_TOKEN_SECRET)
+    if api is None:
+        print("API error")
+        return None
+    print("API created")
+    return api
+
+twitter_api = createTwitterAPI()
 
 
