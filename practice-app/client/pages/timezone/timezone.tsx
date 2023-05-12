de6ba87ba101a71853ed94279a6f35733b07752b
arasgungore <sagungoren@yahoo.com.tr>
2023-05-12 18:12:03 +0300

    Updated frontend

--- practice-app/client/pages/timezone/timezone.tsx
+++ practice-app/client/pages/timezone/timezone.tsx
@@ -6,17 +6,23 @@ const TimeZone = () => {
   const [longitude, setLongitude] = useState('');
   const [timezone, setTimezone] = useState('');
   const [currentTime, setCurrentTime] = useState('');
+
+  const [fromTime, setFromTime] = useState('');
+  const [toTime, setToTime] = useState('');
+  const [fromTimezone, setFromTimezone] = useState('');
+  const [toTimezone, setToTimezone] = useState('');
+  
   const [savedConversions, setSavedConversions] = useState([]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const res = await axios.post(`${process.env.BACKEND_URL}/timezone`, {
+      const res = await axios.post(`${process.env.BACKEND_URL}/get_timezone`, {
         latitude,
         longitude,
       });
-      setTimezone(res.data.zoneName);
-      setCurrentTime(new Date(res.data.formatted).toLocaleTimeString());
+      setTimezone(res.data.timezone);
+      setCurrentTime(res.data.date);
     } catch (error) {
       console.error('Error:', error);
     }
@@ -26,10 +32,11 @@ const TimeZone = () => {
     event.preventDefault();
     try {
       const res = await axios.post(`${process.env.BACKEND_URL}/convert_time`, {
-        time: new Date().toISOString().slice(0, 19).replace('T', ' '),
-        from_tz: timezone,
-        to_tz: timezone,
+        time: fromTime,
+        from_tz: fromTimezone,
+        to_tz: toTimezone,
       });
+      setToTime(res.data.converted_time)
       const conversionName = res.data.conversion_name;
       await axios.post(`${process.env.BACKEND_URL}/save_conversion`, {
         conversion_name: conversionName,
@@ -64,87 +71,120 @@ const TimeZone = () => {
   };
 
   return (
-    <body>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
+<body>
+   <div className="container">
+      <div className="row">
+         <div className="col-md-4">
             <h1>Timezone Converter</h1>
             <form onSubmit={handleSubmit}>
-              <div className="form-group">
-                <label>Latitude</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Enter latitude"
-                  value={latitude}
-                  onChange={(event) => setLatitude(event.target.value)}
-                />
-              </div>
-              <div className="form-group">
-                <label>Longitude</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Enter longitude"
-                  value={longitude}
-                  onChange={(event) => setLongitude(event.target.value)}
-                />
-              </div>
-              <button type="submit" className="btn btn-primary" disabled={!latitude || !longitude}>
-                Convert Time
-              </button>
+               <div className="form-group">
+                  <label>Latitude</label>
+                  <input
+                     type="text"
+                     className="form-control"
+                     placeholder="Enter latitude"
+                     value={latitude}
+                     onChange={(event) => setLatitude(event.target.value)}
+                  />
+               </div>
+               <div className="form-group">
+                  <label>Longitude</label>
+                  <input
+                     type="text"
+                     className="form-control"
+                     placeholder="Enter longitude"
+                     value={longitude}
+                     onChange={(event) => setLongitude(event.target.value)}
+                  />
+               </div>
+               <div className="result">
+                  <p>Timezone: {timezone}</p>
+                  <p>Current Time: {currentTime}</p>
+               </div>
+               <button type="submit" className="btn btn-primary">
+               Convert Timezone
+               </button>
             </form>
-            <br />
-          {timezone && currentTime && (
-          <div className="alert alert-success" role="alert">
-            <h4 className="alert-heading">Conversion Result:</h4>
-            <p>
-              The current time in {timezone} is {currentTime}.
-            </p>
-            <hr />
-            <p className="mb-0">
-              <button className="btn btn-primary" onClick={handleSaveConversion}>
+            <form onSubmit={handleSaveConversion}>
+               <div className="form-group">
+                  <label>Input Time</label>
+                  <input
+                     type="text"
+                     className="form-control"
+                     placeholder="Enter from time"
+                     value={fromTime}
+                     onChange={(event) => setFromTime(event.targetvalue)}
+                  />
+               </div>
+               <div className="form-group">
+                  <label>Input Timezone</label>
+                  <input
+                     type="text"
+                     className="form-control"
+                     placeholder="Enter from timezone"
+                     value={fromTimezone}
+                     onChange={(event) => setFromTimezone(event.targetvalue)}
+                  />
+               </div>
+               <div className="form-group">
+                  <label>Output Timezone</label>
+                  <input
+                     type="text"
+                     className="form-control"
+                     placeholder="Enter to timezone"
+                     value={toTimezone}
+                     onChange={(event) => setToTimezone(event.target.value)}
+                  />
+               </div>
+               
+               <div className="row">
+                  <div className="col-md-12">
+                     <div className="result">
+                        <p>Converted Time: {toTime}</p>
+                     </div>
+                  </div>
+               </div>
+               
+               <button type="submit" className="btn btn-primary">
                 Save Conversion
-              </button>
-            </p>
-          </div>
-        )}
-
-        {savedConversions.length > 0 && (
-          <div>
-            <h2>Saved Conversions:</h2>
-            <ul>
-              {savedConversions.map((conv) => (
-                <li key={conv}>
-                  {conv}{' '}
-                  <button className="btn btn-danger" onClick={(event) => handleDeleteConversion(event, conv)}>
-                    Delete
+               </button>
+            </form>
+         </div>
+         <div className="col-md-8">
+            <h1>Saved Conversions</h1>
+            <button className="btn btn-primary" onClick={handleGetSavedConversions}>
+            Get Saved Conversions
+            </button>
+            <ul className="list-group">
+               {savedConversions.map((conversionName) => (
+               <li key={conversionName} className="list-group-item">
+                  {conversionName}
+                  <button
+                     className="btn btn-danger float-right"
+                     onClick={(event) => handleDeleteConversion(event, conversionName)}
+                  >
+                  Delete
                   </button>
-                </li>
-              ))}
+               </li>
+               ))}
             </ul>
-          </div>
-        )}
-
-        <button className="btn btn-secondary" onClick={handleGetSavedConversions}>
-          Get Saved Conversions
-        </button>
+         </div>
       </div>
       <div className="col-md-8">
-        <iframe
-          src={`https://maps.google.com/maps?q=${latitude},${longitude}&z=10&output=embed`}
-          width="100%"
-          height="480"
-          frameBorder="0"
-          style={{ border: 0 }}
-          allowFullScreen=""
-          aria-hidden="false"
-          tabIndex="0"
-        ></iframe>
+        Show on Map
+         <iframe
+         src={`https://maps.google.com/maps?q=${latitude},${longitude}&z=10&output=embed`}
+         width="100%"
+         height="480"
+         frameBorder="0"
+         style={{ border: 0 }}
+         allowFullScreen=""
+         aria-hidden="false"
+         tabIndex="0"
+         ></iframe>
       </div>
-    </div>
-  </div>
-  </body>
+   </div>
+</body>
   );
 };
 
