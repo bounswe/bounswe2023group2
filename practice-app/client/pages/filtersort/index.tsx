7264cc3625b8cb963a5c33ea1d82b742158e204d
burak <rburaksaritas@gmail.com>
2023-05-17 22:48:11 +0300

    Add Get Air Quality Section

--- practice-app/client/pages/filtersort/index.tsx
+++ practice-app/client/pages/filtersort/index.tsx
@@ -1,74 +1,67 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
-export default function FilterSortPage() {
-  const [resources, setResources] = useState(null);
-  const [aqdata, setAqdata] = useState(null);
+export default function GetAQData() {
+    const [country, setCountry] = useState("");
+    const [city, setCity] = useState("");
+    const [sort, setSort] = useState("asc");
+    const [aqData, setAqData] = useState(null);
 
-  const ResourceCard = (resourceItem) => {
-    let item = resourceItem.resourceItem
+    const fetchData = async () => {
+        try {
+            const response = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/filtersort/aqdata?country=${country}&city=${city}&sort=${sort}`);
+            setAqData(response.data);
+        } catch (error) {
+            console.error("Error fetching data", error);
+            setAqData(null);
+        }
+    };
+
+    const inputStyle = {
+        margin: '0.5rem 0',
+        padding: '0.5rem',
+        width: '200px',
+        color: "black"
+    };
+
+    const buttonStyle = {
+        padding: '0.5rem 1rem',
+        margin: '1rem 1rem',
+        backgroundColor: '#007BFF',
+        color: '#fff',
+        border: 'none',
+        borderRadius: '4px',
+        cursor: 'pointer'
+    };
+
+    const dataContainerStyle = {
+        margin: '1rem 0',
+        padding: '1rem',
+        border: '1px solid #ddd',
+        borderRadius: '4px',
+        backgroundColor: '#f9f9f9',
+        color: "black"
+    };
+
     return (
-      <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-5">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.type}</h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{item.location}</p>
-        <button onClick={() => deleteResource(item.id)} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
-          Delete
-        </button>
-      </div>
-    )
-  };
-
-  const deleteResource = async (resourceId) => {
-    const response = await axios.delete(`/api/resources/${resourceId}`);
-    if(response.status === 200) {
-      setResources(resources.filter(resource => resource.id !== resourceId));
-    } else {
-      console.error(`Error: ${response.status}`);
-    }
-  };
-
-  const getResources = async () => {
-    const response = await axios.get('/api/resources');
-    if(response.status === 200) {
-      setResources(response.data);
-    } else {
-      console.error(`Error: ${response.status}`);
-    }
-  };
-
-  const getAqdata = async () => {
-    const response = await axios.get('/api/aqdata');
-    if(response.status === 200) {
-      setAqdata(response.data);
-    } else {
-      console.error(`Error: ${response.status}`);
-    }
-  };
-
-  useEffect(() => {
-    getResources();
-    getAqdata();
-  }, []);
-
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="flex flex-row justify-center flex-wrap pt-20">
-        {resources == null ? <>loading</> :
-          resources.map((resourceItem, index) => {
-            return (
-              <ResourceCard key={index} resourceItem={resourceItem} ></ResourceCard>
-            );
-          })}
-      </div>
-
-      <div className="flex flex-row justify-center flex-wrap pt-20">
-        {aqdata == null ? <>loading</> :
-          aqdata.map((aqdataItem, index) => {
-            return (
-              <ResourceCard key={index} resourceItem={aqdataItem} ></ResourceCard>
-            );
-          })}
-      </div>
-    </main>
-  );
+        <div>
+            <h1>Get Air Quality Data</h1>
+            <label> Country Code </label>
+            <input style={inputStyle} type="text" value={country} onChange={e => setCountry(e.target.value)} placeholder="TR" />
+            <label> City Name </label>
+            <input style={inputStyle} type="text" value={city} onChange={e => setCity(e.target.value)} placeholder="İstanbul" />
+            <label> Sort by Last Update Direction </label>
+            <select style={inputStyle} value={sort} onChange={e => setSort(e.target.value)}>
+                <option value="asc">Ascending</option>
+                <option value="desc">Descending</option>
+            </select>
+            <button style={buttonStyle} onClick={fetchData}>Fetch Data</button>
+            {aqData && (
+                <div style={dataContainerStyle}>
+                    <h2>Air Quality Data:</h2>
+                    <pre>{JSON.stringify(aqData, null, 2)}</pre>
+                </div>
+            )}
+        </div>
+    );
 }
