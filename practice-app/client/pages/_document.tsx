aaa6f8b749972ff601820203385d9e9fee687270
mervegrbz <merve@aposto.com>
2023-05-28 16:55:56 +0300

    fix CORS error

--- practice-app/client/pages/_document.tsx
+++ practice-app/client/pages/_document.tsx
@@ -1,9 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+
+      </Head>
       <body>
         <Main />
         <NextScript />
