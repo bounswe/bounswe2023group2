4ad96e716c1b8c60412475c487ae5cc424c8118d
mervegrbz <merve@aposto.com>
2023-10-14 21:37:01 +0300

    add main layout

---
+++ Disaster-Response-Platform/frontend/layouts/MainLayout.js
@@ -0,0 +1,50 @@
+
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import styles from "./MainLayout.module.scss";
+import {FaMapMarkedAlt} from "react-icons/fa";
+import {BsSearch} from "react-icons/bs";
+import {CgProfile} from "react-icons/cg";
+function MainLayout({ children }) {
+    const router = useRouter();
+   
+  
+    return (
+        <main className={styles.main}>
+            <div className={styles.navbar}>
+              <div className={styles.leftbar}>
+
+                <nav className={styles.nav}>
+                    <Link href={`/`}>
+                      <h1>DaRP   </h1>
+                    </Link>
+                </nav>
+              </div>
+              <div className={styles.rightbar}>
+                <nav className={styles.nav}>
+                    <Link href={`/map`}>
+                      <FaMapMarkedAlt size={30} />
+                   
+                    </Link>
+                </nav>
+                <nav className={styles.nav}>
+                    <Link href={`search`}>
+                      <BsSearch size={25} />
+                    </Link>
+                </nav>
+                <nav className={styles.nav}>
+                    <Link href={`/profile`}>
+                      <CgProfile size={30} />
+                    </Link>
+                </nav>
+              </div>
+                
+            </div>
+
+            <div className={styles.body}>{children}</div>
+        </main>
+    );
+}
+
+export default MainLayout;
