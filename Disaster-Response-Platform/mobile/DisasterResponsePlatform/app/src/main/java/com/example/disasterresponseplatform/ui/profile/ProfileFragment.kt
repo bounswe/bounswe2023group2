d643511e6ac50052ac84546fa99331f421426a2c
egecan.serbester <egecan.serbester@tokeninc.com>
2023-10-17 11:17:05 +0300

    #291 #trying for smart-commit

--- Disaster-Response-Platform/mobile/DisasterResponsePlatform/app/src/main/java/com/example/disasterresponseplatform/ui/profile/ProfileFragment.kt
+++ Disaster-Response-Platform/mobile/DisasterResponsePlatform/app/src/main/java/com/example/disasterresponseplatform/ui/profile/ProfileFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.disasterresponseplatform.R
 import com.example.disasterresponseplatform.databinding.FragmentProfileBinding
 
 
