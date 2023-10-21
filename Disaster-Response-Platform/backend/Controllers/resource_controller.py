23615902f002a3e9fd8d4379c96ed55b104ed28f
burak <rburaksaritas@gmail.com>
2023-10-21 13:47:44 +0300

    Add resource service layer

--- Disaster-Response-Platform/backend/Controllers/resource_controller.py
+++ Disaster-Response-Platform/backend/Controllers/resource_controller.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException 
 from Models.resource_model import Resource
 import Services.resource_service as resource_service
 
@@ -6,11 +6,16 @@ router = APIRouter()
 
 @router.post("/resource/")
 def create_resource(resource: Resource):
-    return resource_service.create_resource(resource)
+    resource_id = resource_service.create_resource(resource)
+    return {"status": "Resource added successfully", "resource_id": resource_id}
+
 
 @router.get("/resource/{resource_id}")
 def get_resource(resource_id: str):
-    return resource_service.get_resource_by_id(resource_id)
+    resource = resource_service.get_resource_by_id(resource_id)
+    if resource:
+        return resource
+    raise HTTPException(status_code=404, detail="Resource not found")
 
 @router.put("/resource/{resource_id}")
 def update_resource(resource_id: str, resource: Resource):
--- Disaster-Response-Platform/backend/Services/resource_service.py
+++ Disaster-Response-Platform/backend/Services/resource_service.py
@@ -6,17 +6,18 @@ db = client['DisasterResponseLocal']
 resources_collection = db['resources'] 
 
 def create_resource(resource: Resource) -> str:
-    # Insert the resource into the database and return its ID
-    pass
+    result = resources_collection.insert_one(resource.dict())
+    return str(result.inserted_id)
 
 def get_resource_by_id(resource_id: str) -> Resource:
-    # Fetch the resource from the database using its ID
-    pass
+    resource_data = resources_collection.find_one({"_id": resource_id})
+    if resource_data:
+        return Resource(**resource_data)
+    return None
 
 def update_resource(resource_id: str, resource: Resource):
-    # Update the resource in the database
-    pass
+    resources_collection.update_one({"_id": resource_id}, {"$set": resource.dict()})
 
 def delete_resource(resource_id: str):
-    # Delete the resource from the database
-    pass
+    resources_collection.delete_one({"_id": resource_id})
+
