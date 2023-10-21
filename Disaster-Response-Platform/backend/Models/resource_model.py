7ce344f3a8204ff6b46c27e5601cbfe3ff1ec290
burak <rburaksaritas@gmail.com>
2023-10-21 18:51:09 +0300

    Fix merge, remove redundant ObjectId from resource model

--- Disaster-Response-Platform/backend/Models/resource_model.py
+++ Disaster-Response-Platform/backend/Models/resource_model.py
@@ -1,14 +1,13 @@
 from pydantic import BaseModel
 from typing import Dict, Any
 from enum import Enum
-from bson.objectid import ObjectId
 
 class ConditionEnum(str, Enum):
     new = "new"
     used = "used"
 
 class Resource(BaseModel):
-    _id: ObjectId
+    _id: str
     created_by: str
     condition: ConditionEnum
     initialQuantity: int
