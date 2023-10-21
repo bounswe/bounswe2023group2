b0f2af1f28f84292dc1252b54f4d74b0596bd721
burak <rburaksaritas@gmail.com>
2023-10-21 17:10:31 +0300

    Change prefix definition location

--- Disaster-Response-Platform/backend/Controllers/resource_controller.py
+++ Disaster-Response-Platform/backend/Controllers/resource_controller.py
@@ -4,25 +4,25 @@ import Services.resource_service as resource_service
 
 router = APIRouter()
 
-@router.post("/resource/")
+@router.post("/")
 def create_resource(resource: Resource):
     resource_id = resource_service.create_resource(resource)
     return {"status": "Resource added successfully", "resource_id": resource_id}
 
 
-@router.get("/resource/{resource_id}")
+@router.get("/{resource_id}")
 def get_resource(resource_id: str):
     resource = resource_service.get_resource_by_id(resource_id)
     if resource:
         return resource
     raise HTTPException(status_code=404, detail="Resource not found")
 
-@router.put("/resource/{resource_id}")
+@router.put("/{resource_id}")
 def update_resource(resource_id: str, resource: Resource):
     resource_service.update_resource(resource_id, resource)
     return {"status": "Resource updated successfully"}
 
-@router.delete("/resource/{resource_id}")
+@router.delete("/{resource_id}")
 def delete_resource(resource_id: str):
     resource_service.delete_resource(resource_id)
     return {"status": "Resource deleted successfully"}
--- Disaster-Response-Platform/backend/main.py
+++ Disaster-Response-Platform/backend/main.py
@@ -59,7 +59,7 @@ async def check_authorization(request: Request, call_next):
 
 # ROUTES
 app.include_router(authentication_controller.router, prefix="/api/authenticate", tags=["login"])
-app.include_router(resource_controller.router, tags=["resource"])
+app.include_router(resource_controller.router, prefix = "/resource", tags=["resource"])
 
 
 @app.get("/")
