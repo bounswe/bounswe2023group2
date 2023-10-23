17add446407a4d611f67719be2b0572fc5265cbd
HasanBingölbali <74209102+HasanBingolbali@users.noreply.github.com>
2023-10-23 04:03:42 +0300

    Log in sign in function has been implemented
    In here I created ViewModels and binded data and sent demo networkRequests. Whenever Our api is ready, we will revisit and hopefully be able to finish quickly

--- Disaster-Response-Platform/mobile/DisasterResponsePlatform/app/src/main/java/com/example/disasterresponseplatform/data/enums/EndPoint.kt
+++ Disaster-Response-Platform/mobile/DisasterResponsePlatform/app/src/main/java/com/example/disasterresponseplatform/data/enums/EndPoint.kt
@@ -1,6 +1,7 @@
 package com.example.disasterresponseplatform.data.enums
 
 enum class Endpoint(val path: String) {
+    LOGIN("api/login"),
     DATA("joke/any"),
     USER("user"),
     PRODUCTS("products");
