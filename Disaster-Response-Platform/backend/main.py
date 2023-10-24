447f8a8ebcfca2a1c82694446fb4fbb7374a8c18
Begüm Arslan <begumarslan@outlook.de>
2023-10-24 22:06:34 +0300

    deleted older session implementation

--- Disaster-Response-Platform/backend/main.py
+++ Disaster-Response-Platform/backend/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Request, Depends
 from fastapi.middleware.cors import CORSMiddleware
 
-import Services.authentication
 from Controllers import authentication_controller, resource_controller, user_controller
 from fastapi.responses import JSONResponse
 from http import HTTPStatus
@@ -17,46 +16,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# @app.middleware("http")
-# async def check_authorization(request: Request, call_next):
-#     if (not request.url.components.path.startswith("/api/")):
-#         response = await call_next(request)
-#         return response
-
-#     if (request.url.components.path.startswith("/api/authenticate/login")):
-#         response = await call_next(request)
-#         return response
-
-# #Temporary code
-#     # So that the programmers may create users while testing.
-#     # Will be commented out later
-#     if (request.url.components.path.startswith("/api/authenticate/create-user")):
-#         response = await call_next(request)
-#         return response
-
-#     valid_header = False
-#     auth_header_value = request.headers.get('Authorization', None)
-#     bearer_token=""
-#     if auth_header_value:
-#         parts = auth_header_value.split()
-#         if (parts[0].lower() == 'bearer'):
-#             if (len(parts) == 4):
-#                 bearer_token = parts[1]
-#                 if (parts[2].lower() == 'username'):
-#                     user_name = parts[3]
-#                     if (Services.authentication.verify_user_session(user_name, bearer_token)):
-#                         valid_header = True
-
-#     if (valid_header):
-#         response = await call_next(request)
-#         return response
-#     else:
-#         #TODO API calls not requesting authorization will be handled different
-#         content = json.loads(create_json_for_error("Unauthorized  access", "No valid authorization in the API call"))
-#         response = JSONResponse(status_code=HTTPStatus.UNAUTHORIZED,
-#                                 content=content
-#                                 )
-#         return response
 
 # ROUTES
 app.include_router(authentication_controller.router, prefix="/api/authenticate", tags=["login"])
