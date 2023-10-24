349624ad3c5a5223170c41b4f480eebe032f094e
mervegrbz <merve@aposto.com>
2023-10-24 21:55:29 +0300

    session config for iron-session

--- Disaster-Response-Platform/frontend/lib/sessionConfig.js
+++ Disaster-Response-Platform/frontend/lib/sessionConfig.js
@@ -1,8 +1,9 @@
-export default sessionConfig ={
-  cookieName: 'aposto_cookie',
+ const sessionConfig = {
+  cookieName: 'cookie',
   password: 'complex_password_at_least_32_characters_long',
   // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
   cookieOptions: {
     secure: false,
   },
 }
+export default sessionConfig;
