773150734ddc82906426cd16e5fcbac087343447
mehmetkuzu <mehmet.kuzulugil@boun.edu.tr>
2023-10-25 21:33:32 +0300

    very minor code corrections.

--- Disaster-Response-Platform/backend/Controllers/resource_controller.py
+++ Disaster-Response-Platform/backend/Controllers/resource_controller.py
@@ -24,6 +24,7 @@ def create_resource(resource: Resource, response:Response, current_user: str = D
 def get_resource(resource_id: str, response:Response, current_user: str = Depends(authentication_service.get_current_user)):
     try:
         resource = resource_service.get_resource_by_id(resource_id)
+        response.status_code = HTTPStatus.OK
         return json.loads(resource)
     except ValueError as err:
         err_json = create_json_for_error("Resource error", str(err))
@@ -34,6 +35,7 @@ def get_resource(resource_id: str, response:Response, current_user: str = Depend
 def get_all_resources(response:Response, current_user: str = Depends(authentication_service.get_current_user)):
     try:
         resources = resource_service.get_resources()
+        response.status_code = HTTPStatus.OK
         return json.loads(resources)
     except ValueError as err:
         err_json = create_json_for_error("Resource error", str(err))
@@ -45,6 +47,7 @@ def update_resource(resource_id: str, resource: Resource, response:Response, cur
     try:
         updated_resource = resource_service.update_resource(resource_id, resource)
         if updated_resource:
+            response.status_code = HTTPStatus.OK
             return {"resources": [updated_resource]}
         else:
             raise ValueError(f"Resource id {resource_id} not updated")
@@ -64,5 +67,3 @@ def delete_resource(resource_id: str, response:Response, current_user: str = Dep
         err_json = create_json_for_error("Resource update error", str(err))
         response.status_code = HTTPStatus.NOT_FOUND
         return json.loads(err_json)
-
-
