3ac21bae56eb66ae66b9a030c9b4a3cf5d7b49f4
mehmetkuzu <mehmet.kuzulugil@boun.edu.tr>
2023-10-26 18:06:59 +0300

    Very minor correction for api exposes.

--- Disaster-Response-Platform/backend/Controllers/user_controller.py
+++ Disaster-Response-Platform/backend/Controllers/user_controller.py
@@ -77,12 +77,12 @@ async def refresh_access_token(current_user: str  = Depends(authentication_servi
 async def protected_route(current_user: str = Depends(authentication_service.get_current_user)):
     return {"message": f"Welcome, {current_user}!"}
 
-@router.get("/users/me/", response_model=user_model.User)
+#@router.get("/users/me/", response_model=user_model.User)
 async def read_users_me(current_user: user_model.User = Depends(authentication_service.get_current_active_user)):
     return current_user
 
 
-@router.get("/users/me/items/")
+#@router.get("/users/me/items/")
 async def read_own_items(current_user: user_model.User = Depends(authentication_service.get_current_active_user)):
     return [{"item_id": "Foo", "owner": current_user.username}]
 
