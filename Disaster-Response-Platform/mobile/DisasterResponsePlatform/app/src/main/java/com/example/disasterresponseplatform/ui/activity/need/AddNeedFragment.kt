5db391653650fa5fefa728e36974e76bbc63ddab
halilgurbuz <halil.gurbuz@hotmail.com>
2023-10-29 15:59:11 +0300

    # Add Need form: style and functionality improved

--- Disaster-Response-Platform/mobile/DisasterResponsePlatform/app/src/main/java/com/example/disasterresponseplatform/ui/activity/need/AddNeedFragment.kt
+++ Disaster-Response-Platform/mobile/DisasterResponsePlatform/app/src/main/java/com/example/disasterresponseplatform/ui/activity/need/AddNeedFragment.kt
@@ -5,11 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.disasterresponseplatform.R
 import com.example.disasterresponseplatform.databinding.FragmentAddNeedBinding
-import com.example.disasterresponseplatform.databinding.FragmentHomePageBinding
 
 class AddNeedFragment : Fragment() {
 
@@ -19,29 +18,172 @@ class AddNeedFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        binding = FragmentAddNeedBinding.inflate(inflater, container,false)
+        binding = FragmentAddNeedBinding.inflate(inflater, container, false)
+        setUpNeedTypeSpinner()
+        submitAddNeed()
+        return binding.root
+    }
 
-        // Get the array of need categories from resources
-        val needCategory = resources.getStringArray(R.array.need_categories)
-        val needFoodType = resources.getStringArray(R.array.need_food_types)
+
+    private fun setUpNeedTypeSpinner() {
+
+        // Get the array of need types from needs
+        val needTypeArray = resources.getStringArray(R.array.need_types)
 
         // Create an ArrayAdapter using the string array and a default spinner layout
-        val needCategoryAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, needCategory)
-        val needFoodTypeAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, needFoodType)
-
-        // Specify the layout to use when the list of choices appears
-        needCategoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        needFoodTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        val needTypeAdapter =
+            ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_dropdown_item_1line,
+                needTypeArray
+            )
 
         // Apply the adapter to the spinner
-        val spNeedCategory = binding.spNeedCategory
-        spNeedCategory.adapter = needCategoryAdapter
+        val spNeedType = binding.spNeedType
+        spNeedType.setAdapter(needTypeAdapter)
 
-        val spNeedFoodType = binding.spNeedFoodTypes
-        spNeedFoodType.adapter = needFoodTypeAdapter
+        spNeedType.setOnItemClickListener { _, _, position, _ ->
+            // Update the data of the sub-type spinner based on the selected type
+            setUpNeedSubTypeSpinner(needTypeArray[position])
+        }
+    }
 
-        return binding.root
+
+    private fun setUpNeedSubTypeSpinner(selectedType: String) {
+
+        // Get the array of need sub-types
+        val needEmptyArray = resources.getStringArray(R.array.need_empty_sub_types)
+        val needFoodArray = resources.getStringArray(R.array.need_food_sub_types)
+
+        val needEmptyAdapter =
+            ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_dropdown_item_1line,
+                needEmptyArray
+            )
+
+        val needFoodAdapter =
+            ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_dropdown_item_1line,
+                needFoodArray
+            )
+
+        // Apply the adapter to the spinner
+        val spNeedSubType = binding.spNeedSubType
+        val boxNeedSubType = binding.boxNeedSubType
+
+        if (selectedType.isEmpty()) {
+            boxNeedSubType.visibility = View.GONE
+            boxNeedSubType.hint = "Select Need Sub-Type"
+            spNeedSubType.setText("")
+            spNeedSubType.setAdapter(needEmptyAdapter)
+        } else if (selectedType == "Food") {
+            boxNeedSubType.visibility = View.VISIBLE
+            boxNeedSubType.hint = "Select $selectedType Sub-Type"
+            spNeedSubType.setAdapter(needFoodAdapter)
+        } else {
+            boxNeedSubType.visibility = View.VISIBLE
+            boxNeedSubType.hint = "Select $selectedType Sub-Type"
+            spNeedSubType.setText("")
+            spNeedSubType.setAdapter(needEmptyAdapter)
+        }
+    }
+
+    private fun submitAddNeed() {
+        binding.btnSubmit.setOnClickListener {
+            if ((validateFullName() and validatePhoneNumber() and validateQuantity() and validateLocation()) and validateType() and validateSubType()) {
+                Toast.makeText(context, "{'created_by': ${binding.etFullName.editText?.text.toString().trim()}, 'quantity': ${binding.etQuantity.editText?.text.toString().trim()}, 'type':  ${binding.boxNeedType.editText?.text.toString().trim()}}", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, "Check the Fields", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun validateType(): Boolean {
+        val spNeedType = binding.spNeedType
+        val boxNeedType = binding.boxNeedType
+
+        return if (spNeedType.text.isEmpty()) {
+            boxNeedType.error = "Field can not be empty"
+            false
+        } else {
+            boxNeedType.error = null
+            boxNeedType.isErrorEnabled = false
+            true
+        }
+    }
+
+    private fun validateSubType(): Boolean {
+        val spNeedSubType = binding.spNeedSubType
+        val boxNeedSubType = binding.boxNeedSubType
+
+        return if (spNeedSubType.text.isEmpty()) {
+            boxNeedSubType.error = "Field can not be empty"
+            false
+        } else {
+            boxNeedSubType.error = null
+            boxNeedSubType.isErrorEnabled = false
+            true
+        }
+    }
+
+
+    private fun validateLocation(): Boolean {
+        val etLocation = binding.etLocation
+        val location = etLocation.editText?.text.toString().trim()
+
+        return if (location.isEmpty()) {
+            etLocation.error = "Field can not be empty"
+            false
+        } else {
+            etLocation.error = null
+            etLocation.isErrorEnabled = false
+            true
+        }
+    }
+
+    private fun validateQuantity(): Boolean {
+        val etQuantity = binding.etQuantity
+        val quantity = etQuantity.editText?.text.toString().trim()
+
+        return if (quantity.isEmpty()) {
+            etQuantity.error = "Field can not be empty"
+            false
+        } else {
+            etQuantity.error = null
+            etQuantity.isErrorEnabled = false
+            true
+        }
+    }
+
+    private fun validateFullName(): Boolean {
+        val etFullName = binding.etFullName
+        val fullName = etFullName.editText?.text.toString().trim()
+
+        return if (fullName.isEmpty()) {
+            etFullName.error = "Field can not be empty"
+            false
+        } else {
+            etFullName.error = null
+            etFullName.isErrorEnabled = false
+            true
+        }
+    }
+
+    private fun validatePhoneNumber(): Boolean {
+        val etPhoneNumber = binding.etPhoneNumber
+        val phoneNumber = etPhoneNumber.editText?.text.toString().trim()
+
+        return if (phoneNumber.isEmpty()) {
+            etPhoneNumber.error = "Field can not be empty$phoneNumber"
+            false
+        } else {
+            etPhoneNumber.error = null
+            etPhoneNumber.isErrorEnabled = false
+            true
+        }
     }
 }
