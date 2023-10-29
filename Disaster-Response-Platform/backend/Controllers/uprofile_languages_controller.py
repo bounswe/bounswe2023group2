e958dddb750a997d85768ae2f6ed45482dfa2617
mehmetkuzu <mehmet.kuzulugil@boun.edu.tr>
2023-10-29 15:26:59 +0300

    Some decorations as example on language profiles. Depends clause corrected with user -> username.

--- Disaster-Response-Platform/backend/Controllers/uprofile_languages_controller.py
+++ Disaster-Response-Platform/backend/Controllers/uprofile_languages_controller.py
@@ -11,7 +11,7 @@ router = APIRouter()
 
 
 @router.get("/languages", )
-async def get_user_and_language_level(response: Response, anyuser:str= None, language:str = None, current_username: str = Depends(authentication_service.get_current_user)):
+async def get_user_and_language_level(response: Response, anyuser:str= None, language:str = None, current_username: str = Depends(authentication_service.get_current_username)):
     """
     Get a user's language skills OR users (with their language level) with a given language OR current user's language skills
 
@@ -20,15 +20,67 @@ async def get_user_and_language_level(response: Response, anyuser:str= None, lan
     - **anyuser**: A User's name
     - **language**: Language to be searched for
     - **current_username**; Automatically given by token. Used for authorization either.
+    - Note that:
+        - if anyuser is None and language is set, a list of users (in Language info model) will be retured
+        - if anyuser is set but language is None, the language skills of anyuser will be returned
 
-    Note that:
-    - if anyuser is None and language is set, a list of users (in Language info model) will be retured
-    - if anyuser is set but language is None, the language skills of anyuser will be returned
     **Returns**
+
     In a structured json
-    - **usarname**
+    - **username**
     - **language**: Any language like German, Spanish, Zulu etc.
     - **language_level**: One of     beginner, intermediate, advanced. native
+
+    Like:
+    ~~~
+    {
+        "user_languages": [
+            {
+                "username": "mehmetk",
+                "language": "Turkish",
+                "language_level": "native"
+            }
+        ]
+    }
+    ~~~
+    Note that any result with no data will return with success but will be an empty array. Like:
+    ~~~
+    {
+        "user_languages": []
+    }
+    ~~~
+
+    **Error conditions**
+
+    The responce code of the API result is set according to the error.
+
+    Possible codes for structured response for now is:
+    - **HTTPStatus.NOT_FOUND** (404) <br>
+    And the result is formatted like<br>
+    ~~~
+    {
+      "ErrorMessage": "Login required",
+      "ErrorDetails": "Login to create resources"
+    }
+    ~~~
+
+    - **HTTPStatus.UNPROCESSABLE_ENTITY** (422)<br>
+    This is FastAPI response when parameters do not comply with the data model<br>
+    The result is formatted like<br>
+    ~~~
+    {
+      "detail": [
+        {
+          "loc": [
+            "string",
+            0
+          ],
+          "msg": "string",
+          "type": "string"
+        }
+      ]
+    }
+    ~~~
     """
     if anyuser is None:
         if language is None:
@@ -48,7 +100,7 @@ async def get_user_and_language_level(response: Response, anyuser:str= None, lan
 
 
 @router.post("/languages/add-language", )
-async def add_a_language_currentuser(user_language: UserLanguage, response: Response, anyuser:str= None, current_username: str = Depends(authentication_service.get_current_user)):
+async def add_a_language_currentuser(user_language: UserLanguage, response: Response, anyuser:str= None, current_username: str = Depends(authentication_service.get_current_username)):
     """
     Add a language skill to the current user. If the language skill is allready set for the user, level is updated (if different)
 
@@ -59,6 +111,7 @@ async def add_a_language_currentuser(user_language: UserLanguage, response: Resp
         - **language_level**: One of     beginner, intermediate, advanced. native
 
     **Returns**
+
     - **Language Info**: As set by the operation
     """
 
@@ -73,7 +126,7 @@ async def add_a_language_currentuser(user_language: UserLanguage, response: Resp
         return json.loads(err_json)
 
 @router.delete("/languages/delete-language", )
-async def delete_current_users_language(user_language: UserLanguage, response: Response, anyuser:str= None, current_username: str = Depends(authentication_service.get_current_user)):
+async def delete_current_users_language(user_language: UserLanguage, response: Response, anyuser:str= None, current_username: str = Depends(authentication_service.get_current_username)):
     """
     Delete a language skill of the current user
 
