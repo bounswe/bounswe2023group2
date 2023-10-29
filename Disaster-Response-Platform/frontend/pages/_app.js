949d6c4070cdb496312c552f0c87a0c5d7ad4bb4
mervegrbz <merve@aposto.com>
2023-10-29 16:22:38 +0300

    add ui provider

--- Disaster-Response-Platform/frontend/pages/_app.js
+++ Disaster-Response-Platform/frontend/pages/_app.js
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
+import {NextUIProvider} from "@nextui-org/react";
+
+
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
-  return ( getLayout( <Component { ...pageProps}  /> ))
+  return ( 
+    <NextUIProvider>
+      {getLayout( <Component { ...pageProps}  /> )}
+    </NextUIProvider>
+    
+    )
 }
