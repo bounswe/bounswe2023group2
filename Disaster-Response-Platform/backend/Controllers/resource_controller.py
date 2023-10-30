55d37d628d19b4d26c7cbb62a4ccbf537d696e32
burak <rburaksaritas@gmail.com>
2023-10-30 14:18:08 +0300

    Add additional methods

--- Disaster-Response-Platform/backend/Controllers/resource_controller.py
+++ Disaster-Response-Platform/backend/Controllers/resource_controller.py
@@ -9,6 +9,7 @@ from Services.build_API_returns import create_json_for_error
 
 # Change history:
 # 26/10/2023 The changes for auth2 authorization added and merged
+# 30/10/2023 Add additional methods
 
 router = APIRouter()
 
@@ -59,8 +60,6 @@ def update_resource(resource_id: str, resource: Resource, response:Response, cur
         err_json = create_json_for_error("Resource error", str(err))
         response.status_code = HTTPStatus.NOT_FOUND
         return json.loads(err_json)
-
-
     
 @router.delete("/{resource_id}")
 def delete_resource(resource_id: str, response:Response, current_user: str = Depends(authentication_service.get_current_username)):
@@ -70,5 +69,71 @@ def delete_resource(resource_id: str, response:Response, current_user: str = Dep
         return json.loads(res_list)
     except ValueError as err:
         err_json = create_json_for_error("Resource update error", str(err))
+        response.status_code = HTTPStatus.NOT_FOUND
+        return json.loads(err_json)
+
+@router.put("/{resource_id}/initial_quantity")
+def set_initial_quantity_of_resource(resource_id: str, quantity: int, response: Response, current_user: str = Depends(authentication_service.get_current_username)):
+    try:
+        resource_service.set_initial_quantity(resource_id, quantity)
+        response.status_code = HTTPStatus.OK
+        return {"message": f"Initial quantity of resource {resource_id} set to {quantity}"}
+    except ValueError as err:
+        err_json = create_json_for_error("Resource error", str(err))
+        response.status_code = HTTPStatus.NOT_FOUND
+        return json.loads(err_json)
+
+@router.get("/{resource_id}/initial_quantity")
+def get_initial_quantity_of_resource(resource_id: str, response: Response, current_user: str = Depends(authentication_service.get_current_username)):
+    try:
+        quantity = resource_service.get_initial_quantity(resource_id)
+        response.status_code = HTTPStatus.OK
+        return {"initialQuantity": quantity}
+    except ValueError as err:
+        err_json = create_json_for_error("Resource error", str(err))
+        response.status_code = HTTPStatus.NOT_FOUND
+        return json.loads(err_json)
+
+@router.put("/{resource_id}/current_quantity")
+def set_current_quantity_of_resource(resource_id: str, quantity: int, response: Response, current_user: str = Depends(authentication_service.get_current_username)):
+    try:
+        resource_service.set_current_quantity(resource_id, quantity)
+        response.status_code = HTTPStatus.OK
+        return {"message": f"Current quantity of resource {resource_id} set to {quantity}"}
+    except ValueError as err:
+        err_json = create_json_for_error("Resource error", str(err))
+        response.status_code = HTTPStatus.NOT_FOUND
+        return json.loads(err_json)
+
+@router.get("/{resource_id}/current_quantity")
+def get_current_quantity_of_resource(resource_id: str, response: Response, current_user: str = Depends(authentication_service.get_current_username)):
+    try:
+        quantity = resource_service.get_current_quantity(resource_id)
+        response.status_code = HTTPStatus.OK
+        return {"currentQuantity": quantity}
+    except ValueError as err:
+        err_json = create_json_for_error("Resource error", str(err))
+        response.status_code = HTTPStatus.NOT_FOUND
+        return json.loads(err_json)
+
+@router.put("/{resource_id}/condition")
+def set_condition_of_resource(resource_id: str, condition: ConditionEnum, response: Response, current_user: str = Depends(authentication_service.get_current_username)):
+    try:
+        resource_service.set_condition(resource_id, condition)
+        response.status_code = HTTPStatus.OK
+        return {"message": f"Condition of resource {resource_id} set to {condition}"}
+    except ValueError as err:
+        err_json = create_json_for_error("Resource error", str(err))
+        response.status_code = HTTPStatus.NOT_FOUND
+        return json.loads(err_json)
+
+@router.get("/{resource_id}/condition")
+def get_condition_of_resource(resource_id: str, response: Response, current_user: str = Depends(authentication_service.get_current_username)):
+    try:
+        condition = resource_service.get_condition(resource_id)
+        response.status_code = HTTPStatus.OK
+        return {"condition": condition}
+    except ValueError as err:
+        err_json = create_json_for_error("Resource error", str(err))
         response.status_code = HTTPStatus.NOT_FOUND
         return json.loads(err_json)
