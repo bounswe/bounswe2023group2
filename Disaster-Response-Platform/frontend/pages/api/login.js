778c65e818fb6762469d86f0945b947e3d8754c7
Can <canboraugur@gmail.com>
2023-10-30 23:56:26 +0300

    properly save access token

--- Disaster-Response-Platform/frontend/pages/api/login.js
+++ Disaster-Response-Platform/frontend/pages/api/login.js
@@ -8,8 +8,8 @@ export default withIronSessionApiRoute(
       const { username, password } = JSON.parse(req.body);
       const { data } = await api.post('/api/users/login', { "username_or_email_or_phone": username, password });
       req.session.user = {
-        accessToken: data?.payload?.tokens?.accessToken,
-        refreshToken: data?.payload?.tokens?.refreshToken,
+        accessToken: data?.access_token,
+        refreshToken: data?.refresh_token,
         languages: 'tr'
       };
       await req.session.save();
