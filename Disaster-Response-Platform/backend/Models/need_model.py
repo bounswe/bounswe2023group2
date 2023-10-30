30c9d7c721a6e5f79471b78981df21b0ee0094f6
burak <rburaksaritas@gmail.com>
2023-10-31 00:11:42 +0300

    Add coordinates

--- Disaster-Response-Platform/backend/Models/need_model.py
+++ Disaster-Response-Platform/backend/Models/need_model.py
@@ -11,6 +11,8 @@ class Need(BaseModel):
     unsuppliedQuantity: int = Field(default=None)
     type: str = Field(default=None)
     details: Dict[str, Any] = Field(default=None)
+    x: float = Field(default=0.0)
+    y: float = Field(default=0.0)
     
 class QuantityUpdate(BaseModel):
     quantity: int
