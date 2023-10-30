1792159c214f6b3c56171bbf8c530e0701f18b06
mehmetkuzu <mehmet.kuzulugil@boun.edu.tr>
2023-10-30 23:43:50 +0300

    A dummy user created and login for a start

--- Disaster-Response-Platform/backend/Tests/test_user_profile.py
+++ Disaster-Response-Platform/backend/Tests/test_user_profile.py
@@ -7,25 +7,16 @@ from Models.user_profile_model import *
 from http import HTTPStatus
 client = TestClient(app)
 
-def any_user_create():
-    result:CreateUserRequest = CreateUserRequest(username = "testi", first_name="Mahir", last_name = "Testeden",
+user_to_create:CreateUserRequest = CreateUserRequest(username = "testi", first_name="Mahir", last_name = "Testeden",
                                                  phone_number = "05325325353", is_email_verified = False, private_account = False,
                                                  password = "tesTi2023!", email = "testi@boun.edu.tr")
-    return result
+correct_login_request :LoginUserRequest = LoginUserRequest(username_or_email_or_phone = "testi", password = "tesTi2023!")
+language_to_add:UserLanguage = UserLanguage(username=None, language="Zulu", language_level="native")
 
-def user_login():
-    result:LoginUserRequest = LoginUserRequest(username_or_email_or_phone = "testi", password = "tesTi2023!")
-    return result
+def test_create_user():
 
-def user_language():
-    result:UserLanguage = UserLanguage(username=None, language="Zulu", language_level="native")
-    return result
-def atest_create_user():
-    # url_params = {"username": "mehmetk", "password": "CMPE451"}
-    sign_user = any_user_create()
-    response = client.post("/api/users/signup", json=dict(sign_user))
+    response = client.post("/api/users/signup", json=dict(user_to_create))
 
-    print(response.json())
     assert response.status_code == HTTPStatus.CREATED
 
     user_id = response.json()["inserted_id"]
@@ -34,10 +25,8 @@ def atest_create_user():
     return user_id
 
 def test_login():
-    login_user = user_login()
-    response = client.post("/api/users/login", json=dict(login_user))
+    response = client.post("/api/users/login", json=dict(correct_login_request))
 
-    print(response.json())
     assert response.status_code == HTTPStatus.OK
 
     token = response.json()["access_token"]
@@ -47,14 +36,11 @@ def test_login():
 
 def test_create_profile():
     token = test_login()
-    language = user_language()
 
-    response = client.post("/api/profiles/languages/add-language", json=dict(language))
+    response = client.post("/api/profiles/languages/add-language",
+                           json=dict(language_to_add),
+                           headers={"Authorization": f"Bearer {token}"})
 
-    print(response.json())
     assert response.status_code == HTTPStatus.OK
 
-    token = response.json()["access_token"]
-
-    assert token is not None
-    return token
+    return response.json()
