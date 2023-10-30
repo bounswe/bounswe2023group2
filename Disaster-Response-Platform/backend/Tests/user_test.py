34ba7895a1bd5e3d07a763faf1ee04bd48155905
Begüm Arslan <begumarslan@outlook.de>
2023-10-30 21:37:52 +0300

    added additional signup tests

--- Disaster-Response-Platform/backend/Tests/user_test.py
+++ Disaster-Response-Platform/backend/Tests/user_test.py
@@ -7,32 +7,32 @@ db = MongoDB.getInstance()
 
 
 correct_signup_body= {
-  "username": "ne354631",
+  "username": "begummm",
   "password": "a2345678",
-  "email": "b423um2@ou2tl21ook.com",
+  "email": "begumarslan@outlook.de",
   "disabled": False,
   "first_name": "Begum",
   "last_name": "Arslan",
-  "phone_number": "05345783149",
+  "phone_number": "05531420999",
   "is_email_verified": False,
   "private_account": True
 }
 
 missing_field_signup_body= {
-  "username": "begum",
-  "password": "a2345678",
-  "email": "begum@outlook.com",
+  "username": "mistake",
+  "password": "a234567we8",
+  "email": "begum@outwelook.com",
   "disabled": False,
   "first_name": "Begum",
   "last_name": "Arslan",
-  "phone_number": "05345783149",
+  "phone_number": "05355783149",
   "is_email_verified": False,
   "private_account": False
 }
 wrong_phone_number_signup_body= {
-  "username": "begum",
+  "username": "begu45m",
   "password": "a2345678",
-  "email": "begum@outlook.com",
+  "email": "begu45m@outlook.com",
   "disabled": False,
   "first_name": "Begum",
   "last_name": "Arslan",
@@ -43,9 +43,9 @@ wrong_phone_number_signup_body= {
 
 
 only_email_signup_body= {
-  "username": "230420832begum",
+  "username": "begum_email",
   "password": "a2345678",
-  "email": "begm@ou22.com",
+  "email": "emailsignup@ou22.com",
   "disabled": False,
   "first_name": "Begum",
   "last_name": "Arslan",
@@ -54,40 +54,60 @@ only_email_signup_body= {
 }
 
 only_min_fields_signup_body= {
-  "username": "begum",
+  "username": "begum4",
   "password": "a2345678",
-  "email": "begum@outlook.com",
+  "email": "begum4@outlook.com",
   "first_name": "Begum",
   "last_name": "Arslan"
 }
 
 short_password_signup_body= {
-  "username": "begum",
+  "username": "begum5",
   "password": "a2345",
-  "email": "begum@outlook.com",
+  "email": "begum5@outlook.com",
   "first_name": "Begum",
   "last_name": "Arslan"
 }
 
 digitless_password_signup_body= {
-  "username": "begum",
+  "username": "begum6",
   "password": "asjkhdkshd",
-  "email": "begum@outlook.com",
+  "email": "begum6@outlook.com",
   "first_name": "Begum",
   "last_name": "Arslan"
 }
 
 wrong_email_signup_body= {
-  "username": "begum",
+  "username": "begum7",
   "password": "asjkhdkshd",
-  "email": "begumoutlook.com",
+  "email": "begumoutlook7.com",
   "first_name": "Begum",
   "last_name": "Arslan"
+}
+
+username_login_body= {
+    "username": "begum",
+    "password": "a2345678"
+
+}
+
+email_login_body= {
+    "username": "begum",
+    "password": "a2345678"
+
+}
+
+phone_login_body= {
+    "username": "begum",
+    "password": "a2345678"
+
 }
 def test_signup1():
     response = client.post("/api/users/signup", json=
     correct_signup_body)
     assert response.status_code == 201
+
+
 
 def test_signup2():
     response = client.post("/api/users/signup", json=
@@ -104,3 +124,37 @@ def test_signup4():
     response = client.post("/api/users/signup", json=
     only_email_signup_body)
     assert response.status_code == 201
+
+def test_signup5():
+    response = client.post("/api/users/signup", json=
+    only_min_fields_signup_body)
+    assert response.status_code == 201
+
+
+def test_signup6():
+    response = client.post("/api/users/signup", json=
+   short_password_signup_body)
+    assert response.status_code == 422
+
+def test_signup7():
+    response = client.post("/api/users/signup", json=
+    digitless_password_signup_body)
+    assert response.status_code == 401
+
+def test_signup7():
+    response = client.post("/api/users/signup", json=
+    wrong_email_signup_body)
+    assert response.status_code == 422
+
+
+
+
+
+# def test_login1():
+#     response = client.post("/api/users/login", json=
+#     correct_signup_body)
+#     assert response.status_code == 201
+# def test_login2():
+#     response = client.post("/api/users/login", json=
+#     correct_signup_body)
+#     assert response.status_code == 201
