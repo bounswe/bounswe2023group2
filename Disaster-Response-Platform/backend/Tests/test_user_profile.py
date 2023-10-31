797c78b1309a92c1179edb2b03081c02ae4c8329
mehmetkuzu <mehmet.kuzulugil@boun.edu.tr>
2023-10-31 10:14:38 +0300

    A short start for prepare test logic. Pushed to catch with main.

--- Disaster-Response-Platform/backend/Tests/test_user_profile.py
+++ Disaster-Response-Platform/backend/Tests/test_user_profile.py
@@ -1,30 +1,32 @@
-import json
-
 from fastapi.testclient import TestClient
 from main import app
 from Models.user_model import *
 from Models.user_profile_model import *
 from http import HTTPStatus
+from Database.mongo import MongoDB
 client = TestClient(app)
 
 user_to_create:CreateUserRequest = CreateUserRequest(username = "testi", first_name="Mahir", last_name = "Testeden",
                                                  phone_number = "05325325353", is_email_verified = False, private_account = False,
                                                  password = "tesTi2023!", email = "testi@boun.edu.tr")
 correct_login_request :LoginUserRequest = LoginUserRequest(username_or_email_or_phone = "testi", password = "tesTi2023!")
+incorrect_login_request :LoginUserRequest = LoginUserRequest(username_or_email_or_phone = "testi", password = "INCORRECT")
 language_to_add:UserLanguage = UserLanguage(username=None, language="Zulu", language_level="native")
 
-def test_create_user():
+def prepare_for_test():
+    profile_languages = MongoDB.get_collection('user_languages')
+    profile_optional_infos = MongoDB.get_collection('user_optional_infos')
+    profile_professions = MongoDB.get_collection('user_professions')
+    profile_skills = MongoDB.get_collection('user_skills')
+    profile_socialmedias = MongoDB.get_collection('user_socialmedias')
+    return
 
+def create_user():
     response = client.post("/api/users/signup", json=dict(user_to_create))
+    return
 
-    assert response.status_code == HTTPStatus.CREATED
 
-    user_id = response.json()["inserted_id"]
-
-    assert user_id is not None
-    return user_id
-
-def test_login():
+def do_login():
     response = client.post("/api/users/login", json=dict(correct_login_request))
 
     assert response.status_code == HTTPStatus.OK
@@ -34,8 +36,10 @@ def test_login():
     assert token is not None
     return token
 
+
 def test_create_profile():
-    token = test_login()
+    create_user()
+    token = do_login()
 
     response = client.post("/api/profiles/languages/add-language",
                            json=dict(language_to_add),
