87497c12b8d54bc7fe070c4bff310229c6de384a
mervegrbz <merve@aposto.com>
2023-10-31 11:34:27 +0300

    add needs

--- Disaster-Response-Platform/frontend/components/ActivityTable.js
+++ Disaster-Response-Platform/frontend/components/ActivityTable.js
@@ -2,116 +2,60 @@ import React from "react";
 
 
 
-export default function ActivityTable() {
-  return (
-    
-<div class="w-full overflow-x-auto shadow-md sm:rounded my-10">
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-white dark:bg-slate-400 dark:text-black">
-            <tr>
-                <th scope="col" class="px-6 py-5">
-                    Product name
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Color
-                    <a href="#"><svg class="w-3 h-3 ml-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-  </svg></a>
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Category
-                    <a href="#"><svg class="w-3 h-3 ml-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-  </svg></a>
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Price
-                    <a href="#"><svg class="w-3 h-3 ml-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-  </svg></a>
-                </th>
-                
-            </tr>
-        </thead>
-        <tbody>
-            <tr class="bg-white border-b dark:bg-gray-100 dark:border-gray-500">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">
-                    Apple MacBook Pro 17"
-                </th>
-                <td class="px-6 py-4">
-                    Silver
-                </td>
-                <td class="px-6 py-4">
-                    Laptop
-                </td>
-                <td class="px-6 py-4">
-                    $2999
-                </td>
-              
-            </tr>
-            <tr class="border-b bg-gray-50 dark:bg-gray-200 dark:border-gray-500">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">
-                    Microsoft Surface Pro
-                </th>
-                <td class="px-6 py-4">
-                    White
-                </td>
-                <td class="px-6 py-4">
-                    Laptop PC
-                </td>
-                <td class="px-6 py-4">
-                    $1999
-                </td>
-               
-            </tr>
-            <tr class="bg-white border-b dark:bg-gray-100 dark:border-gray-500">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">
-                    Magic Mouse 2
-                </th>
-                <td class="px-6 py-4">
-                    Black
-                </td>
-                <td class="px-6 py-4">
-                    Accessories
-                </td>
-                <td class="px-6 py-4">
-                    $99
-                </td>
-               
-            </tr>
-            <tr class="border-b bg-gray-50 dark:bg-gray-200 dark:border-gray-500">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">
-                    Google Pixel Phone
-                </th>
-                <td class="px-6 py-4">
-                    Gray
-                </td>
-                <td class="px-6 py-4">
-                    Phone
-                </td>
-                <td class="px-6 py-4">
-                    $799
-                </td>
-                
-            </tr>
-            <tr>
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">
-                    Apple Watch 5
-                </th>
-                <td class="px-6 py-4">
-                    Red
-                </td>
-                <td class="px-6 py-4">
-                    Wearables
-                </td>
-                <td class="px-6 py-4">
-                    $999
-                </td>
-                
-            </tr>
-        </tbody>
-    </table>
-</div>
+export default function ActivityTable({ resource }) {
+    return (
 
-  );
+        <div class="w-full overflow-x-auto shadow-md sm:rounded my-10">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-white dark:bg-slate-400 dark:text-black">
+                    <tr>
+                        <th scope="col" class="px-6 py-5">
+                            Type
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Location
+                            <a href="#"><svg class="w-3 h-3 ml-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                            </svg></a>
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Created by
+                            <a href="#"><svg class="w-3 h-3 ml-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                            </svg></a>
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Details
+                            <a href="#"><svg class="w-3 h-3 ml-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                            </svg></a>
+                        </th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    {resource.map((resource) => (
+                        <>
+                            <tr class="bg-white border-b dark:bg-gray-100 dark:border-gray-500">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">
+                                    {resource.type}
+                                </th>
+                                <td class="px-6 py-4">
+                                    {resource.condition}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {resource.created_by}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {resource.details.subtype ?? resource.details.tool_type }
+                                </td>
+                            </tr>
+                        </>
+                    ))}
+                   
+                </tbody>
+            </table>
+        </div>
+
+    );
 }
--- Disaster-Response-Platform/frontend/pages/index.js
+++ Disaster-Response-Platform/frontend/pages/index.js
@@ -2,18 +2,36 @@ import styles from '@/styles/Home.module.css'
 import MainLayout from '@/layouts/MainLayout'
 import dynamic from 'next/dynamic'
 import ActivityTable from '@/components/ActivityTable'
+import { toast } from 'react-toastify'
+import { useEffect, useState } from 'react'
 
 const Map = dynamic(() => import('../components/Map/Map'), {
   ssr: false,
 })
 
 export default function home() {
+  const [resource, setResource] = useState([]); 
+  const resources = async () => {
+    const  response = await  fetch('/api/resource', { method: 'GET', headers: { "Content-Type": "application/json" }});
+  let res = await response.json();
+     if (response.ok) {
+
+      setResource(res.resources)
+    } else {
+      // unknown error
+      toast.error("An unexpected error occurred while saving, please try again")
+    }
+  }
+  useEffect(()=>{
+    resources();
+  
+    
+  },[resource])
+
   return (
     <>
         <Map />
-        <ActivityTable />
-     
-     
+        <ActivityTable resource={resource} />
       <div className={styles.buttonContainer}>
       </div>
     </>
