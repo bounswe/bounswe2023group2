0ae13e0c8a27fef53ec2cfcb5fb95fd369377cd2
mervegrbz <merve@aposto.com>
2023-11-21 15:50:24 +0300

    feat(backend): added sample s3 bucket upload and get url

---
+++ Disaster-Response-Platform/backend/Database/s3.py
@@ -0,0 +1,32 @@
+import boto3
+from pprint import pprint
+import pathlib
+import os
+import config
+def upload_file_using_client():
+    """
+    Uploads file to S3 bucket using S3 client object
+    :return: None
+    """
+    s3 = boto3.client("s3", 
+                aws_access_key_id=config.AWS_ACCESS_KEY_ID,
+                aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY)
+    bucket_name = 'files.practice-app.online'
+    object_name = "deneme.txt"
+    file_name = os.path.join(pathlib.Path(__file__).parent.resolve(), "deneme.txt")
+
+    response = s3.upload_file(file_name, bucket_name, object_name)
+    
+def get_file(file):
+
+  s3 = boto3.client("s3", 
+                aws_access_key_id='AKIAZ5XGZRMHMYTNYB6T',
+                aws_secret_access_key='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')
+  bucket_name = 'files.practice-app.online'
+  object_name = "deneme.txt"
+  file_name = os.path.join(pathlib.Path(__file__).parent.resolve(), "deneme.txt")
+  print(f'url: https://files.practice-app.online/{file}')
+  # response = s3.upload_file(file_name, bucket_name, object_name)
+
+# upload_file_using_client()
+get_file('deneme.txt')
