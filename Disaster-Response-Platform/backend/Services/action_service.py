7c831d38ec6da090aa99de1190dccaccfe5b3c0f
Begüm Arslan <begumarslan@outlook.de>
2023-11-23 22:36:14 +0300

    added create_Action service

--- Disaster-Response-Platform/backend/Services/action_service.py
+++ Disaster-Response-Platform/backend/Services/action_service.py
@@ -4,7 +4,8 @@ from Database.mongo import MongoDB
 from bson.objectid import ObjectId
 
 from Services.build_API_returns import *
-
+from Services.resource_service import *
+from Services.need_service import *
 
 # Get the resources collection using the MongoDB class
 actions_collection = MongoDB.get_collection('actions')
@@ -18,14 +19,47 @@ def create_action(action: Action) -> str:
     ## eklerken type ı need_resoruce match ise , related needs ve related resources empty mi diye check ederim
     # 
     if not all([action.created_by,
-                action.details, action.x, action.y, action.needs, action.resources]):
+                action.type]):
         raise ValueError("All fields are mandatory for creation.")
-    insert_result = actions_collection.insert_one(action.dict())
+    if (action.type== ActionType.need_resource):
+        if(not all([action.endLocation_x,action.start_location_x, action.start_location_y, action.endLocation_y])):
+            raise ValueError("All fields are mandatory for creation.")
+        #action ın need ve resource larına bakalım while içinde listin tüm elemanlarını tarverse edip need ve resource un recur untilinden uzun olamasın
+        # burda recurrence ı anlayınca tüm seriyi listeye ekleyeyim en iyisi
+        # buraya need ve resource type , detail check koyalım aynı type ta değilse hata versin
+        min_datetime = datetime.datetime.max
+        for id in action.related_resources:
+            related_resource = resources_collection.find_one({"_id": ObjectId(id)})
+            if(related_resource.recurrence_id is not None):
+                action.recurrence=True
+                if(min_datetime> related_resource.recurrence_deadline):
+                    min_datetime=related_resource.recurrence_deadline
+        for id in action.related_needs:
+            related_need= needs_collection.find_one({"_id": ObjectId(id)})
+            if(related_need.recurrence_id is not None):
+                action.recurrence=True
+                if(min_datetime> related_need.recurrence_deadline):
+                    min_datetime=related_need.recurrence_deadline
+        if(action.end_at > min_datetime):
+            raise ValueError("Action end time should be set properly")
+        
+        insert_result = actions_collection.insert_one(action.dict())
+   
     #check the result to change from
     if insert_result.inserted_id:
-        ActionSuccess(action_id= insert_result.inserted_id)
+        return ActionSuccess(action_id= str(insert_result.inserted_id))
     else:
         raise ValueError("Action could not be created")
+
+def do_action(action_id:str, current_user:str):
+    action = actions_collection.find_one({"_id": ObjectId(action_id)})
+    if(action.created_by!= current_user ):
+        raise ValueError("Only user created the action can do it")
+    #burda da aynı date teki aynı tipteki resource ve needleri birleştireyim (RESOURCE u eksiltelim, need i karşılayalım)
+    # resource need den az ise: 
+    # TODO (aynı date te birden fazla resource caseini inceler)
+    
+
     
 
 def get_action_by_id(action_id: str) -> list[dict]:
