91e234baaa94eb3e0d59ee18d7317c0b1d46c0fb
Begüm Arslan <begumarslan@outlook.de>
2023-11-25 18:51:11 +0300

    implemented do action service with recurrence

--- Disaster-Response-Platform/backend/Services/action_service.py
+++ Disaster-Response-Platform/backend/Services/action_service.py
@@ -16,97 +16,301 @@ def create_action(action: Action) -> str:
     # Manual validation for required fields during creation
     #can there be actions without a resource
 
-    ## todo burayı düzelt koçum
-    ## eklerken type ı need_resoruce match ise , related needs ve related resources empty mi diye check ederim
-    # 
     if not all([action.created_by,
                 action.type]):
         raise ValueError("All fields are mandatory for creation.")
     if (action.type== ActionType.need_resource):
-        if(not all([action.endLocation_x,action.start_location_x, action.start_location_y, action.endLocation_y])):
+        if not action.related_groups:
             raise ValueError("All fields are mandatory for creation.")
+        #if(not all([action.endLocation_x,action.start_location_x, action.start_location_y, action.endLocation_y])):
+        #    raise ValueError("All fields are mandatory for creation.")
         #action ın need ve resource larına bakalım while içinde listin tüm elemanlarını tarverse edip need ve resource un recur untilinden uzun olamasın
         # burda recurrence ı anlayınca tüm seriyi listeye ekleyeyim en iyisi
         # buraya need ve resource type , detail check koyalım aynı type ta değilse hata versin
         i=0
         for group in action.related_groups:
-
-            min_datetime = datetime.now(timezone.utc)
-        
-
-            #to check resource type and need type match
+            if not group.related_resources:
+                raise ValueError("Please enter resource id")
+            def normalize_date(dt):
+                return datetime(dt.year, dt.month, dt.day)
             id= group.related_resources[0]
             resource=resources_collection.find_one({"_id": ObjectId(id)})
-            print(resource)
+
+            #TODO resource need active mi check i ekle
             type= resource["type"]
             id_list=[]
             for id in group.related_resources:
-                print(id)
+      
                 related_resource = resources_collection.find_one({"_id": ObjectId(id)})
                 if related_resource["type"] != type:
                     raise ValueError("You can only take action between resource and needs that has the same type")
-      
-                print("debug2",related_resource["created_by"])
-                print("groups",action.related_groups)
+                if not related_resource["active"]:
+                    raise ValueError("You have entered a inactive need id")
+
                 if(related_resource.get("recurrence_id") is not None):
                     #find all recurrences with r_id 
-                    print("resource recurrence id is not none")
-                    query = {"recurrence_id": related_resource["recurrence_id"]}
-                    matching_resources = resources_collection.find(query, {"_id": 1})
-                    rid_list = [str(resource["_id"]) for resource in matching_resources]
-                    id_list.extend(rid_list)
-                  
-                    if(min_datetime> related_resource["recurrence_deadline"]):
-                        min_datetime=related_resource["recurrence_deadline"]
+                    id_list.extend(get_resource_list_by_id(id))
+                    if( normalize_date(related_resource["recurrence_deadline"]) < normalize_date(action.end_at)):
+                        raise ValueError("Action end date cant be longer than the resource recurrence deadline")
+                else:
+                    id_list.append(id)
+                    if( not (normalize_date(related_resource["occur_at"]) <= normalize_date(action.end_at)) and (normalize_date(related_resource["occur_at"])>= normalize_date(action.occur_at)) ):
+                        #print(normalize_date(related_resource["occur_at"]),normalize_date(action.end_at), normalize_date(related_resource["occur_at"]),normalize_date(action.occur_at)  )
+                        raise ValueError("Action and resource should occur at the same date")
             #add recuuring resource list to action's mentioned resource list
-            action.related_groups[i].related_resources.extend(id_list)
+            action.related_groups[i].related_resources= id_list
+            id_list=[]
             for id in group.related_needs:
-                print(id)
-                print("groups",action.related_groups)
+                if not group.related_resources:
+                    raise ValueError("Please enter need ids")
                 if related_resource["type"] != type:
                     raise ValueError("You can only take action between resource and needs that has the same type")
+                if not related_resource["active"]:
+                    raise ValueError("You have entered a inactive resource id")
                 related_need= needs_collection.find_one({"_id": ObjectId(id)})
                 if(related_need.get("recurrence_id") is not None):
-                    print("need recurrence id is not none")
-                    print(related_need.get("recurrence_id"))
-                    print("end date of action",action.end_at)
-                    print("min date_Time",min_datetime)
-                    #find all recurrences with r_id 
-                    query = {"recurrence_id": related_need["recurrence_id"]}
-                    matching_needs = needs_collection.find(query, {"_id": 1})
-                    nid_list = [str(need["_id"]) for need in matching_needs]
-                    id_list.extend(nid_list)
-                    #add recuuring resource list to action's mentioned resource list
-                    
-                    #TODO burda date leri kıyaslayamıyom kafayı yiycem
-                    # if(min_datetime> related_need["recurrence_deadline"]):
-                    #     min_datetime=related_need["recurrence_deadline"]
-                    
-            action.related_groups[i].related_needs.extend(id_list)
+                    id_list.extend(get_need_list_by_id(id))
+    
+                    if(normalize_date(related_need["recurrence_deadline"]) < normalize_date(action.end_at)):
+                        raise ValueError("Action end date cant be longer than the need recurrence deadline")
+                else:
+                    id_list.append(id)
+                    if( not(normalize_date(related_need["occur_at"]) <= normalize_date(action.end_at) and normalize_date(related_need["occur_at"])>= normalize_date(action.occur_at)) ):
+                        raise ValueError("Action and need should occur at the same date")     
+            action.related_groups[i].related_needs=(id_list)
             i+=1
-        print("end date of action",action.end_at)
-        if(action.end_at is not None):
-            if(action.end_at > min_datetime):
-                raise ValueError("Action end time should be set properly")
-        
         insert_result = actions_collection.insert_one(action.dict())
    
-    #check the result to change from
-    if insert_result.inserted_id:
-        return ActionSuccess(action_id= str(insert_result.inserted_id))
-    else:
-        raise ValueError("Action could not be created")
+        #check the result to change from
+        if insert_result.inserted_id:
+            return ActionSuccess(action_id= str(insert_result.inserted_id))
+        else:
+            raise ValueError("Action could not be created")
+    
 
 def do_action(action_id:str, current_user:str):
     action = actions_collection.find_one({"_id": ObjectId(action_id)})
-    if(action.created_by!= current_user ):
+    if not action:
+        raise ValueError("There is no action with this id")
+    
+    if(action['created_by']!= current_user ):
         raise ValueError("Only user created the action can do it")
+    groups= action["related_groups"]
+    met_needs=[]
+    for group in groups:
+        
+        related_needs= group['related_needs']
+        related_resources= group['related_resources']
+        #get need objects
+        current_date = action['occur_at']
+        totalQuantityMet=0
+        #for each action day query for that days need and resources
+        while(current_date <= action['end_at']):
+            # Query to find documents with matching IDs and occur_at time
+            end_date= current_date + timedelta(days=1)
+            start_date= current_date- timedelta(days=1)
+            needs= []
+            for id in related_needs:
+                query = {
+                "_id":  ObjectId(id),
+                'occur_at': current_date,
+                'active':True
+                }
+                need= needs_collection.find_one(query)
+                if need :
+                    needs.append(need)
+
+            if len(needs)!=0:
+                need= needs[0]
+                checkTotalQuantity(related_needs,related_resources, current_date, need['type'])
+                #get resource objects that occurs at date of action
+                related_resources_object_ids = [ObjectId(id_str) for id_str in related_resources]
+                query = {
+                    "_id": {"$in": related_resources_object_ids},
+                    'occur_at': {'$gte': current_date, '$lt': end_date},
+                    'active': True
+                }
+                
+                resources= resources_collection.find(query)
+                resources= list(resources)
+                r=0
+                i=0
+                if len(resources)!=0:
+                    while(i<len(needs) and r< len(resources)):
+                        resource= resources[r]
+                        need= needs[i]
+                        if need['unsuppliedQuantity']<resource['currentQuantity']:
+                            left= resource['currentQuantity']- need['unsuppliedQuantity']
+                            totalQuantityMet+= need['unsuppliedQuantity']
+                            update_need(need, False, left)
+                            
+                            i+=1
+                            
+                        elif need['unsuppliedQuantity']>resource['currentQuantity']:
+                            left= need['unsuppliedQuantity']-resource['currentQuantity']
+                            update_resource(resource, False, left)  
+                            r+=1
+                        else:
+                            left=0
+                            totalQuantityMet+= need['unsuppliedQuantity']
+                            update_need(need, False, left)
+                            update_resource(resource, False,left)
+                            i+=1
+                            r+=1
+                    
+                    if len(needs)<= i:
+                        i-=1
+                    if len(resources)<=r:
+                        r-=1
+                    if resources[r]['active']==False:
+                        totalQuantityMet+= need['unsuppliedQuantity']
+                        update_need(need, True, left)
+                    if needs[i]['active']==False:
+                        update_resource(resource, True, left)
+            current_date += timedelta(days=1) #this can be optimized to min recurrence rate
+        if(totalQuantityMet==0):
+            raise ValueError("There is either no need for this resource or no resource for this need")
+        met_needs.append(totalQuantityMet)
+
+    ret= doActionResponse(met_needs=met_needs)
+    return ret
+
+def update_need(need, active, left):
+    need['active']=active
+    q=left
+    if not active:
+        q=0
+    update_result = needs_collection.update_one(
+        {"_id": need['_id']},
+        {"$set": {"active": active, "unsuppliedQuantity": q}}
+    )
+    return
+def update_resource(resource, active, left):
+    resource['active']=active
+    q=left
+    if not active:
+        q=0
+    update_result = resources_collection.update_one(
+        {"_id": resource['_id']},
+        {"$set": {"active": active, "currentQuantity": q}}
+    )
+    return
+
+
     #burda da aynı date teki aynı tipteki resource ve needleri birleştireyim (RESOURCE u eksiltelim, need i karşılayalım)
     # resource need den az ise: 
     # TODO (aynı date te birden fazla resource caseini inceler)
+    # TODO farklı date lerdeki totali bulmuşuum yanlış olmuş
+def checkTotalQuantity(related_needs,related_resources, currentdate, type):
+    related_needs_object_ids = [ObjectId(id_str) for id_str in related_needs]
+    related_resources_object_ids = [ObjectId(id_str) for id_str in related_resources]
+    #print("total chck", related_needs)
+    query = {
+        "_id": {"$in": related_needs_object_ids},
+        "occur_at": currentdate
+    }
+
+    needs_cursor = needs_collection.find(query)
+    needs = list(needs_cursor)
+
+    # MongoDB aggregation pipeline to sum unsuppliedQuantity
+    pipeline = [
+        {"$match": query},
+        {"$group": {
+            "_id": None,
+            "total_unsupplied_quantity": {"$sum": "$unsuppliedQuantity"}
+        }}
+    ]
+    # Execute the aggregation pipeline
+    result = needs_collection.aggregate(pipeline)
+    # Extract the result
+    total_unsupplied_quantity = next(result, {"total_unsupplied_quantity": 0})["total_unsupplied_quantity"]
+   
+    query = {
+        "_id": {"$in": related_resources_object_ids},
+        "occur_at": currentdate
+    }
+    # MongoDB aggregation pipeline to sum unsuppliedQuantity
+    pipeline = [
+        {"$match": query},
+        {"$group": {
+            "_id": None,
+            "total_currentQuantity": {"$sum": "$currentQuantity"}
+        }}
+    ]
+    # Execute the aggregation pipeline
+    result = resources_collection.aggregate(pipeline)
+
+    # Extract the result
+    total_currentQuantity = next(result, {"total_currentQuantity": 0})["total_currentQuantity"]
+    if total_unsupplied_quantity> total_currentQuantity:
+        raise ValueError(f"There is not enough resource for this {type} need at {currentdate}")
+    else:
+        return total_unsupplied_quantity
     
 
-    
+def get_group_info(related_group: ActionGroup):
+    related_needs= related_group.related_needs
+    related_resources= related_group.related_resources
+    id= related_resources[0]
+    resource=resources_collection.find_one({"_id": id})
+    if not resource:
+        raise ValueError(f"Cant found the resource with id: {id}")
+
+    type= resource["type"]
+    resourceTexts=[]
+    needTexts=[]
+    for id in related_resources:
+        related_resource = resources_collection.find_one({"_id": ObjectId(id)})
+        if not related_resource:
+            raise ValueError(f"Cant found the reosurce with id: {id}")
+        if related_resource["type"] != type:
+            raise ValueError("You can only take action between resource and needs that has the same type")
+        if(related_resource.get("recurrence_id") is not None):
+            resourceTexts.append(f"From {related_resource['occur_at']}, to {related_resource['recurrence_deadline']} e {type} resource" )
+        else:
+            resourceTexts.append(f"At {related_resource['occur_at']} {type} resource")
+    for id in related_needs:
+        related_need = needs_collection.find_one({"_id": ObjectId(id)})
+        if not related_need:
+            raise ValueError(f"Cant found the need with id: {id}")
+        if related_need["type"] != type:
+            raise ValueError("You can only take action between resource and needs that has the same type")
+        if(related_need.get("recurrence_id") is not None):
+            needTexts.append(f"From {related_need['occur_at']}, to {related_need['recurrence_deadline']} {type} need for every {related_need['recurrence_rate']} day" )
+        else:
+            needTexts.append(f"At {related_need['occur_at']} {type} need")
+    ret= groupCheckResponse(type=type,resourceTexts=resourceTexts, needTexts=needTexts)
+    return ret
+
+
+
+def get_resource_list_by_id(resource_id: str):
+    resource = resources_collection.find_one({"_id": ObjectId(resource_id)})
+    if not resource:
+        raise ValueError("This error does not exist")
+    if(resource.get("recurrence_id") is not None):
+        #find all recurrences with r_id 
+        query = {"recurrence_id": resource["recurrence_id"]}
+        matching_resources = resources_collection.find(query, {"_id": 1})
+        nid_list = [str(resource["_id"]) for resource in matching_resources]
+        return nid_list
+    else: 
+        return resource_id
+
+
+def get_need_list_by_id(need_id: str):
+    need = needs_collection.find_one({"_id": ObjectId(need_id)})
+    if not need:
+        raise ValueError("This error does not exist")
+    if(need.get("recurrence_id") is not None):
+        #find all recurrences with r_id 
+        query = {"recurrence_id": need["recurrence_id"]}
+        matching_needs= needs_collection.find(query, {"_id": 1})
+        nid_list = [str(need["_id"]) for need in matching_needs]
+        return nid_list
+    else: 
+        return need_id
 
 def get_action_by_id(action_id: str) -> list[dict]:
     return get_actions(action_id)
@@ -129,8 +333,7 @@ def get_actions(action_id:str = None) -> list[dict]:
                   "last_updated_at":1,
                   "upvote":1,
                   "downvote":1,
-                  "related_needs":1,
-                  "related_resources":1,
+                  "related_groups":1,
                   "end_at":1
                   }
     #projection["_id"] = {"$toString": "$_id"}
