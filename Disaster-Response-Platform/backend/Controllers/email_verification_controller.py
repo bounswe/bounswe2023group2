a8b440c13c40e7b3d11ab45ccc8eaf947d16f23d
burak <rburaksaritas@gmail.com>
2023-12-12 03:45:02 +0300

    Implement email verification controller

--- Disaster-Response-Platform/backend/Controllers/email_verification_controller.py
+++ Disaster-Response-Platform/backend/Controllers/email_verification_controller.py
@@ -1,11 +1,20 @@
-# imports
+from fastapi import APIRouter, Depends, HTTPException, Body
+from Services.email_verification_service import send_verification_email, verify_user
+from Services.authentication_service import get_current_username, get_current_email
 
 router = APIRouter()
 
 @router.post("/send", status_code=200)
-async def send_verification_email(username: str = Depends(authentication_service.get_current_username)):
-    return 
+async def send_verification_request(username: str = Depends(get_current_username), email: str = Depends(get_current_email)):
+    success = send_verification_email(username, email)
+    if success:
+        return {"message": "Verification email sent."}
+    else:
+        raise HTTPException(status_code=500, detail="Failed to send verification email")
 
 @router.post("/verify", status_code=200)
-async def verify_email():
-    return
+async def verify_email(token: str = Query(...), username: str = Depends(get_current_username)):
+    if verify_user(username, token):
+        return {"message": "Email verified successfully."}
+    else:
+        raise HTTPException(status_code=400, detail="Invalid token or token expired")
