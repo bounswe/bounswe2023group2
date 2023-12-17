b3467bea431dc625d518dd9faee06f8f6bc21761
burak <rburaksaritas@gmail.com>
2023-12-17 14:31:27 +0300

    Remove authentication check

--- Disaster-Response-Platform/backend/Controllers/geocode_controller.py
+++ Disaster-Response-Platform/backend/Controllers/geocode_controller.py
@@ -4,8 +4,7 @@ from Services.geocode_service import autocomplete_address_and_convert_to_coordin
 router = APIRouter()
 
 @router.get("/address_to_coordinates",)
-def autocomplete_address(input_address: str = Query(..., description="Input address to autocomplete"), 
-                        current_user: str = Depends(authentication_service.get_current_username)):
+def autocomplete_address(input_address: str = Query(..., description="Input address to autocomplete")):
     """
     Autocompletes the input address and returns the complete address along with its coordinates.
     """
@@ -25,8 +24,7 @@ def autocomplete_address(input_address: str = Query(..., description="Input addr
 
 @router.get("/coordinates_to_address")
 def convert_coordinates(latitude: float = Query(..., description="Latitude (x) of the location"),
-                        longitude: float = Query(..., description="Longitude (y) of the location"),
-                        current_user: str = Depends(authentication_service.get_current_username)):
+                        longitude: float = Query(..., description="Longitude (y) of the location")):
     """
     Converts latitude and longitude to a formatted address.
     """
