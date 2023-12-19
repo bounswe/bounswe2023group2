ffef5cc56fa35c4293624d4030aed0c0a5bc8276
Begüm Arslan <begumarslan@outlook.de>
2023-12-19 21:59:19 +0300

    added regex searches for need and resource

--- Disaster-Response-Platform/backend/Controllers/search_controller.py
+++ Disaster-Response-Platform/backend/Controllers/search_controller.py
@@ -27,3 +27,48 @@ async def search_users(response:Response,query: str, current_user: UserProfile =
     
 
     return  userList
+
+
+
+# @router.get("/actions/{query}", responses={
+#     status.HTTP_200_OK: {"model": SearchResults},
+#     status.HTTP_403_FORBIDDEN: {"model": Error}
+# })
+# async def search_actions(response:Response,query: str, current_user: UserProfile = Depends(authentication_service.get_current_user)):
+#     userList = search_service.search_users(query)
+    
+
+#     return  userList
+
+
+# @router.get("/events/{query}", responses={
+#     status.HTTP_200_OK: {"model": SearchResults},
+#     status.HTTP_403_FORBIDDEN: {"model": Error}
+# })
+# async def search_events(response:Response,query: str, current_user: UserProfile = Depends(authentication_service.get_current_user)):
+#     userList = search_service.search_events(query)
+    
+
+#     return  userList
+
+
+@router.get("/needs/{query}", responses={
+    status.HTTP_200_OK: {"model": SearchResults},
+    status.HTTP_403_FORBIDDEN: {"model": Error}
+})
+async def search_events(response:Response,query: str, current_user: UserProfile = Depends(authentication_service.get_current_user)):
+    userList = search_service.search_needs(query)
+    
+
+    return  userList
+
+
+@router.get("/resources/{query}", responses={
+    status.HTTP_200_OK: {"model": SearchResults},
+    status.HTTP_403_FORBIDDEN: {"model": Error}
+})
+async def search_resources(response:Response,query: str, current_user: UserProfile = Depends(authentication_service.get_current_user)):
+    userList = search_service.search_resources(query)
+    
+
+    return  userList
--- Disaster-Response-Platform/backend/Services/search_service.py
+++ Disaster-Response-Platform/backend/Services/search_service.py
@@ -5,12 +5,15 @@ from fastapi import HTTPException, Depends, status
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from Models.user_model import *
+from Models.resource_model import *
+from Models.need_model import *
 from typing import Annotated
 import config
 
 #roleRequestsDb = MongoDB.get_collection('role_verification_requests')
 userDb = MongoDB.get_collection('authenticated_user')
-
+reource_collection = MongoDB.get_collection('resources')
+need_collection = MongoDB.get_collection('needs')
 
 def search_users(query: str)-> List[dict]:
     #if username is given
@@ -25,3 +28,35 @@ def search_users(query: str)-> List[dict]:
     results = [UserInfo(**document) for document in cursor]
     return results
     
+
+
+def search_resources(query: str)-> List[dict]:
+    #if username is given
+    cursor = reource_collection.find({
+        "$or": [
+            {"created_by": {"$regex": query, "$options": "i"}},
+            {"description": {"$regex": query, "$options": "i"}},
+            {"type": {"$regex": query, "$options": "i"}},
+            {"details.subtype": {"$regex": query, "$options": "i"}},
+            {"details.type": {"$regex": query, "$options": "i"}}
+            # Add other fields if needed
+        ]
+    })
+    results = [Resource(**document) for document in cursor]
+    return results
+    
+
+def search_needs(query: str)-> List[dict]:
+    #if username is given
+    cursor = need_collection.find({
+        "$or": [
+            {"created_by": {"$regex": query, "$options": "i"}},
+            {"description": {"$regex": query, "$options": "i"}},
+            {"type": {"$regex": query, "$options": "i"}},
+            {"details.subtype": {"$regex": query, "$options": "i"}},
+            {"details.type": {"$regex": query, "$options": "i"}}
+            # Add other fields if needed
+        ]
+    })
+    results = [Need(**document) for document in cursor]
+    return results
