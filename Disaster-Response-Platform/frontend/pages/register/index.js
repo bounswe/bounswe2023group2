7fb3850ef0f8671aba9653bbbf68f4148e7584f3
mervegrbz <merve@aposto.com>
2023-12-19 11:39:56 +0300

    feat(frontend): refactor authentication pages, add reset password

--- Disaster-Response-Platform/frontend/pages/register/index.js
+++ Disaster-Response-Platform/frontend/pages/register/index.js
@@ -2,49 +2,49 @@
 import MainLayout from '@/layouts/MainLayout'
 import { Button, Input } from '@nextui-org/react';
 import { useForm } from 'react-hook-form';
-import { toast } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import { useRouter } from 'next/navigation'
 import { withIronSessionSsr } from 'iron-session/next';
 import sessionConfig from '@/lib/sessionConfig';
 import getLabels from '@/lib/getLabels';
-
+import Image from 'next/image';
+import dapp_logo_extended_white from "../../public/logo/dapp_logo_main_blue.svg";
 export default function register({ labels }) {
   const router = useRouter()
   const { register, reset, handleSubmit, setError, formState: { isSubmitting, errors } } = useForm();
 
 
   const onSubmit = async (data) => {
+    console.log(data)
     const  response = await fetch('/api/register', { method: 'POST',
     headers: {
       "Content-Type": "application/json",
     }, body: JSON.stringify(data) });
-    
-    if (response.status === 400) {
-      const fieldToErrorMessage = await response.json()
-      for (const [fieldName, errorMessage] of Object.entries(fieldToErrorMessage)) {
-        setError(fieldName, { type: 'custom', message: errorMessage })
-      }
-    } else if (response.ok) {
+    console.log(response)
+   if (response.status == 200) {
       // successful
       toast.success(labels.feedback.save_success);
       router.push("/")
     } else {
       // unknown error
-      toast.error(labels.feedback.failure);
+      toast.error(`${labels.feedback.failure} ${response?.data?.ErrorDetail}`);
     }
   }
-
-
   const fields = [
     { type: "text", name: "username", required: true },
     { type: "text", name: "first_name", required: true },
     { type: "text", name: "last_name", required: true  },
     { type: "password", name: "password", required: true },
-    { type: "phone_number", name: "phone_number", required: true },
-    { type: "email", name: "email", required: true },
+    { type: "phone_number", name: "phone_number", required: false },
+    { type: "email", name: "email", required: false },
 ]
-  return <form className="rounded-xl bg-gray-200 p-10 w-8/12" onSubmit={handleSubmit(onSubmit)} >
-    <h1 className='text-6xl font-normal leading-normal mt-0 mb-1 text-center text-emerald-800'>{labels.auth.register}</h1>
+  return <form className='flex flex-col border-2 m-5 p-10 ' onSubmit={handleSubmit(onSubmit)} >
+    <Image
+              src={dapp_logo_extended_white}
+              alt="Logo"
+              width={150}
+              className='self-center'
+            />
           {fields.map(field => {
               return <>
                   <Input type={field.type} 
@@ -53,15 +53,18 @@ export default function register({ labels }) {
                       label={labels.profile[field.name]}
                       labelPlacement={'outside'}
                       variant={'faded'}
-                      className='mb-6'
+                      className='w-80 mt-4'
+                      isRequired = {field.required}
                       placeholder={labels.placeholders[field.name] ? labels.placeholders[field.name] : " "}
                   />
                   <div className="error" >{errors[field.name]?.message}</div>
+                  {field.name == 'password' && <span  className='mt-3 text-slate-400' > Please write at least one of the fields: <br/> email or phone number</span> }
               </>
           })}
-          <Button disabled={isSubmitting} type='submit'>
+          <Button disabled={isSubmitting} color='primary' type='submit' className='m-3 self-end mr-0 w-10'>
               {isSubmitting ? labels.UI.loading : labels.UI.submit}
           </Button>
+          <ToastContainer/>
       </form>;
 }
 
