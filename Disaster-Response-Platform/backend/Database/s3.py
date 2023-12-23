a00991117c9a23b0574ef6138b1f9e27d3c75adf
Begüm Arslan <begumarslan@outlook.de>
2023-12-23 21:13:38 +0300

    added downloadfile api

--- Disaster-Response-Platform/backend/Database/s3.py
+++ Disaster-Response-Platform/backend/Database/s3.py
@@ -1,8 +1,12 @@
 import boto3
 from pprint import pprint
+from fastapi import Query, APIRouter, HTTPException, Response, Depends, Body
+from typing import List, Optional
 import pathlib
 import os
 import config
+import json
+from Services.resource_service import *
 
 s3 = boto3.client("s3", 
                 aws_access_key_id=config.AWS_ACCESS_KEY_ID,
@@ -25,3 +29,43 @@ def delete_file_from_s3(file):
     except Exception as e:
         print(e)
         return {"message": "S3_DELETE_ERROR"}
+
+
+def create_and_upload_file(    
+    resource_id: str = None,
+    active: Optional[bool] = None,
+    types: list = None, 
+    subtypes: list = None, 
+    x: float = None,
+    y: float = None,
+    distance_max: float = None,
+    sort_by: str = 'created_at',
+    order: Optional[str] = 'desc'
+
+):
+
+    resources= get_resources(
+            active=active,
+            types=types,
+            subtypes=subtypes,
+            x=x,
+            y=y,
+            distance_max=distance_max,
+            sort_by=sort_by,
+            order=order
+        )
+    resources_json = json.dumps(resources)
+
+    filename = "filtered_resources.json"  # Replace with your desired file name
+    with open(filename, "w") as file:
+        file.write(resources_json)
+
+    # Step 3: Upload the file to S3
+    try:
+        bucket_name = 'files.practice-app.online'
+        response = s3.upload_file(filename, bucket_name, filename)
+        file_url = f'https://{bucket_name}.s3.amazonaws.com/{filename}'
+        return {"url": file_url}
+    except Exception as e:
+        print(e)
+        return {"message": "S3_UPLOAD_ERROR"}
