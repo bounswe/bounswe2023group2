bd2cc63547173028df72a40d8ca0fbc1fd975cc1
mervegrbz <merve@aposto.com>
2023-12-26 14:30:43 +0300

    revert date

--- Disaster-Response-Platform/backend/Models/action_model_v2.py
+++ Disaster-Response-Platform/backend/Models/action_model_v2.py
@@ -65,7 +65,7 @@ class Action(BaseModel):
     def convert_str_to_datetime(cls, value):
         if isinstance(value, str):
             try:
-                return datetime.datetime.strptime(value, '%Y-%m-%d %H:%M')
+                return datetime.datetime.strptime(value, '%Y-%m-%d')
             except ValueError:
                 raise ValueError("Incorrect date format, should be YYYY-MM-DD HH:MM")
         return value
