64e24f05ef35495f0c9129527a4a25d305e50111
azizamankenova <azizamankenova@gmail.com>
2023-12-29 03:41:12 +0300

    admin tests for verify, unverify and unauthorize endpoins are implemented

---
+++ Disaster-Response-Platform/backend/Tests/admin_test.py
@@ -0,0 +1,254 @@
+import sys
+sys.path.append("..")
+from fastapi.testclient import TestClient
+from main import app
+import random
+import json
+import string
+from http import HTTPStatus
+
+from Database.mongo import MongoDB
+# from Services import need_service 
+# from Models import need_model
+client = TestClient(app)
+db = MongoDB.getInstance()
+
+# token can be set
+TOKEN = None
+header = None
+report_id = None
+
+signup_body= {
+  "password": "a2345678",
+  "disabled": False,
+  "first_name": "User",
+  "last_name": "Test",
+  "is_email_verified": False,
+  "private_account": True
+}
+
+email_login_body= {
+    "username_or_email_or_phone": "",
+    "password": "a2345678"
+}
+username_data = {}
+
+
+signup_body2= {
+  "password": "a2345678",
+  "disabled": False,
+  "first_name": "User2",
+  "last_name": "Test2",
+  "is_email_verified": False,
+  "private_account": True
+}
+
+verify_data = {
+  "username": "string"
+}
+need_data = {
+  "description": "string",
+  "initialQuantity": 8,
+  "quantityUnit": "piece",
+  "urgency": 2,
+  "unsuppliedQuantity": 8,
+  "type": "string",
+  "details": {
+        "disease_name": "asthma",
+        "medicine_name": "inhaler",
+        "age": 20
+            },
+  "open_address": "Celiktepe",
+  "x": 20,
+  "y": 50,
+  "occur_at": "2023-12-29"
+}
+
+test_username = ""
+
+report_data = {
+  "description": "This is fake need",
+  "report_type": "needs"
+}
+updated_report_data = {
+    "description": "new description"
+}
+
+
+report_data_wrong = {
+        "description": "string",
+        "report_type": "needs"
+    }
+    
+    
+def generate_random_string(length=10):
+    """Generate a random string of letters and digits."""
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+def generate_random_email():
+    """Generate a random email address."""
+    random_username = generate_random_string()
+    return f"{random_username}@example.com"
+
+def generate_random_phone_number():
+    """Generate a random phone number with the format 05XXXXXXXX."""
+    # Generate a random 8-digit number
+    random_digits = ''.join([str(random.randint(0, 9)) for _ in range(9)])
+    # Return the number with the 05 prefix
+    return "05" + random_digits
+
+
+def test_signup():
+    username = generate_random_string()
+    signup_body['username']  = username
+    username_data['username'] = username
+    signup_body['email']  = generate_random_email()
+    signup_body['phone_number']  = generate_random_phone_number()
+    email_login_body['username_or_email_or_phone']=username
+    response = client.post("/api/users/signup", json=signup_body)    
+    assert response.status_code == 201
+    # return user_id
+
+def test_login():
+    global TOKEN, header
+    response = client.post("/api/users/login", json=email_login_body)
+    assert response.status_code == HTTPStatus.OK
+    print(response.json())
+    TOKEN =  response.json()["access_token"]
+    header = {"Authorization": f"Bearer {TOKEN}"}
+
+
+def test_make_admin():
+    global TOKEN
+    response = client.post(f"/api/userroles/make-admin/?username={username_data['username']}",
+                           headers=header)
+    assert response.status_code == HTTPStatus.CREATED
+    # assert response.json()["message"] == "User role updated"
+    assert response.json()["user_role"] == "ADMIN"
+
+def test_signup2():
+    username = generate_random_string()
+    signup_body2['username']  = username
+    
+    signup_body2['email']  = generate_random_email()
+    signup_body2['phone_number']  = generate_random_phone_number()
+    username_data["username"]=username
+    response = client.post("/api/users/signup", json=signup_body2)    
+    assert response.status_code == 201
+    # return user_id
+
+
+def test_verify_user():
+    
+    global TOKEN
+
+    # if TOKEN is None:
+    #     signup()
+    #     TOKEN = login()
+    token = TOKEN
+        
+
+    response = client.put("/api/users/verify",
+                           json=username_data,
+                           headers=header)
+
+    assert response.status_code == HTTPStatus.OK
+    res = response.json()
+    assert "is verified" in res["message"]
+    
+    
+def test_verify_user2():
+    
+    global TOKEN
+
+    # if TOKEN is None:
+    #     signup()
+    #     TOKEN = login()
+    token = TOKEN
+        
+
+    response = client.put("/api/users/verify",
+                           json={"username": "randomstring"},
+                           headers=header)
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    res = response.json()
+    assert res["ErrorMessage"]=="Verification error"
+    assert "No such user" in res["ErrorDetail"]
+    
+def test_unverify_user():
+    
+    global TOKEN
+
+    # if TOKEN is None:
+    #     signup()
+    #     TOKEN = login()
+    token = TOKEN
+        
+
+    response = client.put("/api/users/unverify",
+                           json=username_data,
+                           headers=header)
+
+    assert response.status_code == HTTPStatus.OK
+    res = response.json()
+    assert "is unverified" in res["message"]
+    
+    
+def test_unverify_user2():
+    
+    global TOKEN
+
+    # if TOKEN is None:
+    #     signup()
+    #     TOKEN = login()
+    token = TOKEN
+        
+
+    response = client.put("/api/users/unverify",
+                           json={"username": "randomstring"},
+                           headers=header)
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    res = response.json()
+    assert res["ErrorMessage"]=="Unverification error"
+    assert "No such user" in res["ErrorDetail"]
+    
+def test_unauthorize_user():
+    
+    global TOKEN
+
+    # if TOKEN is None:
+    #     signup()
+    #     TOKEN = login()
+    token = TOKEN
+        
+
+    response = client.put("/api/users/unauthorize",
+                           json=username_data,
+                           headers=header)
+
+    assert response.status_code == HTTPStatus.OK
+    res = response.json()
+    assert "is unauthorized" in res["message"]
+    
+    
+def test_unauthorize_user2():
+    
+    global TOKEN
+
+    # if TOKEN is None:
+    #     signup()
+    #     TOKEN = login()
+    token = TOKEN
+        
+
+    response = client.put("/api/users/unauthorize",
+                           json={"username": "randomstring"},
+                           headers=header)
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    res = response.json()
+    assert res["ErrorMessage"]=="Unauthorization error"
+    assert "No such user" in res["ErrorDetail"]
+
